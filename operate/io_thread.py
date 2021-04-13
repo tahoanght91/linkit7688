94cfb02e04158327bdc0016097fcc296a878b35b
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-13 22:15:06 +0700

    comment check_check_sum function

--- operate/io_thread.py
+++ operate/io_thread.py
@@ -64,11 +64,11 @@ def _read_data(byte_stream):
     if byte_stream[0] != b'\xa0':
         LOGGER.debug('Mark byte not right, expected mark byte A0, received mark byte %s', byte_stream[0].encode('hex'))
         return False
-    if not check_check_sum(byte_stream, BYTE_ORDER):
-        LOGGER.debug('Check sum not right, expected check sum %s, received check sum %s',
-                with_check_sum(byte_stream[:-2], BYTE_ORDER)[-2:].encode('hex'),
-                byte_stream[-2:].encode('hex'))
-        return False
+    # if not check_check_sum(byte_stream, BYTE_ORDER):
+    #     LOGGER.debug('Check sum not right, expected check sum %s, received check sum %s',
+    #             with_check_sum(byte_stream[:-2], BYTE_ORDER)[-2:].encode('hex'),
+    #             byte_stream[-2:].encode('hex'))
+    #     return False
     frame_length = bytes_to_int(byte_stream[1])
     op_code = byte_stream[2]
     LOGGER.debug('Opcode %s', op_code.encode('hex'))
