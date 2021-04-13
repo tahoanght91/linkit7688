07310941f23abfe23788c3a079ffb97220c848bf
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-13 21:55:44 +0700

    edit theo firmware moi

--- devices/crmu.py
+++ devices/crmu.py
@@ -3,20 +3,10 @@ from .utils import _read_attribute
  
  # fixed crmu to 13 byte
 def extract(byte_data):
-    # card_id = byte_data[0:12].encode('hex')
-    # card_len = bytes_to_int(byte_data[13])
-    # door_state = bytes_to_int(byte_data[14])
-    #
-    # _read_attribute('crmuCardId', card_id)
-    # _read_attribute('card_len', card_len)
-    # _read_attribute('crmuDoorState', door_state)
-
     card_id = byte_data[0:12].encode('hex')
-    door_state = bytes_to_int(byte_data[12])
-
-    online_status = bytes_to_int(byte_data[13])
+    card_len = bytes_to_int(byte_data[13])
+    door_state = bytes_to_int(byte_data[14])
 
     _read_attribute('crmuCardId', card_id)
+    _read_attribute('card_len', card_len)
     _read_attribute('crmuDoorState', door_state)
-
-    _read_attribute('crmuOnlineStatus', online_status)
