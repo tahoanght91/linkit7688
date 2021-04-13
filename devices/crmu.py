75311340c8a9b8d7e8b9ae1a8c4f897d5ab95ea4
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-13 23:18:16 +0700

    edit dinh dang du lieu nhu firmware cu

--- devices/crmu.py
+++ devices/crmu.py
@@ -1,12 +1,15 @@
 from utility import bytes_to_int
 from .utils import _read_attribute
- 
- # fixed crmu to 13 byte
+
+
+# fixed crmu to 13 byte
 def extract(byte_data):
     card_id = byte_data[0:12].encode('hex')
-    card_len = bytes_to_int(byte_data[13])
-    door_state = bytes_to_int(byte_data[14])
+    door_state = bytes_to_int(byte_data[12])
+
+    online_status = bytes_to_int(byte_data[13])
 
     _read_attribute('crmuCardId', card_id)
-    _read_attribute('card_len', card_len)
     _read_attribute('crmuDoorState', door_state)
+
+    _read_attribute('crmuOnlineStatus', online_status)
