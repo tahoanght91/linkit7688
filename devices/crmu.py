21d70474e96462c26f8a744a850f2f7c77ede031
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-13 10:30:36 +0700

    edit crmu.py

--- devices/crmu.py
+++ devices/crmu.py
@@ -4,11 +4,11 @@ from .utils import _read_attribute
  # fixed crmu to 13 byte
 def extract(byte_data):
     card_id = byte_data[0:12].encode('hex')
-    door_state = bytes_to_int(byte_data[12])
+    card_len = bytes_to_int(byte_data[13])
+    door_state = bytes_to_int(byte_data[14])
 
-    online_status = bytes_to_int(byte_data[13])
+    # online_status = bytes_to_int(byte_data[13])
 
     _read_attribute('crmuCardId', card_id)
+    _read_attribute('card_len', card_len)
     _read_attribute('crmuDoorState', door_state)
-
-    _read_attribute('crmuOnlineStatus', online_status)
