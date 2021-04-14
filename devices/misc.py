1736d58d3a165193605a313018f283bdd6fa19df
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-15 00:32:39 +0700

    edit bell.py

--- devices/misc.py
+++ devices/misc.py
@@ -3,11 +3,11 @@ from utility import bytes_to_int
 from .utils import _read_attribute, _read_telemetry
 
 def extract(byte_data):
-    fireState = bytes_to_int(byte_data[0])
-    offOnFire = bytes_to_int(byte_data[1])
-    smokeState = bytes_to_int(byte_data[2])
-    floodState = bytes_to_int(byte_data[3])
-    moveSensor = bytes_to_int(byte_data[4])
+    fireState = bytes_to_int(byte_data[0])   # fireState
+    smokeState = bytes_to_int(byte_data[1])   # smokeState
+    moveSensor = bytes_to_int(byte_data[2])   # moveSensor
+    din3 = bytes_to_int(byte_data[3])
+    din4 = bytes_to_int(byte_data[4])
     din5 = bytes_to_int(byte_data[5])
     din6 = bytes_to_int(byte_data[6])
     din7 = bytes_to_int(byte_data[7])
@@ -19,10 +19,10 @@ def extract(byte_data):
     vbus = bytes_to_int(byte_data[15])
 
     _read_attribute('fireState', fireState)
-    _read_attribute('offOnFire', offOnFire)
     _read_attribute('smokeState', smokeState)
-    _read_attribute('floodState', floodState)
     _read_attribute('moveSensor', moveSensor)
+    _read_attribute('miscDin4', din4)
+    _read_attribute('miscDin3', din3)
     _read_attribute('miscDin5', din5)
     _read_attribute('miscDin6', din6)
     _read_attribute('miscDin7', din7)
--- monitor/bell.py
+++ monitor/bell.py
@@ -1,18 +1,20 @@
 import time
 from config import *
 
+
 def _send_command(command):
     if command == 'on' and client_attributes.get('miscBellState', default_data.miscBellState) == 0:
         commands_lock.acquire()
         commands['bell'] = 'on'
         commands_lock.release()
         return 'on'
-    elif command =='off' and client_attributes.get('miscBellState', default_data.miscBellState) == 1:
+    elif command == 'off' and client_attributes.get('miscBellState', default_data.miscBellState) == 1:
         commands_lock.acquire()
         commands['bell'] = 'off'
         commands_lock.release()
         return 'off'
     return ''
+
 
 def apply():
     now = time.localtime()
@@ -20,18 +22,15 @@ def apply():
         LOGGER.debug('Smoke detected, ring the bell')
     if client_attributes.get('fireState', default_data.fireState):
         LOGGER.debug('Fire detected, ring the bell')
-    if client_attributes.get('floodState', default_data.floodState):
-        LOGGER.debug('Flood detected, ring the bell')
     if client_attributes.get('crmuDoorState', default_data.crmuDoorState) and (now.tm_hour > 17 or now.tm_hour < 7):
         LOGGER.debug('Unexpected door opened, ring the bell')
     if client_attributes.get('moveSensor', default_data.moveSensor) and (now.tm_hour > 17 or now.tm_hour < 7):
         LOGGER.debug('Unexpected motion sensed, ring the bell')
-        
+
     if (client_attributes.get('smokeState', default_data.smokeState)
             or client_attributes.get('fireState', default_data.fireState)
-            or client_attributes.get('floodState', default_data.floodState)
             or client_attributes.get('crmuDoorState', default_data.crmuDoorState)
-                and (now.tm_hour > 17 or now.tm_hour < 7) 
+                and (now.tm_hour > 17 or now.tm_hour < 7)
             or client_attributes.get('moveSensor', default_data.moveSensor)
                 and (now.tm_hour > 17 or now.tm_hour < 7)):
         return _send_command('on')
