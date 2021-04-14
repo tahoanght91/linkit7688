214eff2bb207e315ca63fee4802222a815d353ed
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-15 00:00:42 +0700

    edit apply bell.py

--- monitor/bell.py
+++ monitor/bell.py
@@ -27,7 +27,7 @@ def apply():
     if client_attributes.get('moveSensor', default_data.moveSensor) and (now.tm_hour > 17 or now.tm_hour < 7):
         LOGGER.debug('Unexpected motion sensed, ring the bell')
         
-    if (client_attributes.get('smokeState', default_data.smokeState)
+    if (not client_attributes.get('smokeState', default_data.smokeState)
             or client_attributes.get('fireState', default_data.fireState)
             or client_attributes.get('floodState', default_data.floodState)
             or client_attributes.get('crmuDoorState', default_data.crmuDoorState)
