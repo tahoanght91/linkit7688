e7f291d16f51bcdb25fbaf3e653aa281fd2ef3f6
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-19 11:30:23 +0700

    edit format json telemetry, client attributes, shared_attributes

--- operate/telemetry_thread.py
+++ operate/telemetry_thread.py
@@ -3,28 +3,12 @@ import time
 from config import *
 
 
-# def call():
-#     period = shared_attributes.get('periodSendTelemetry', default_data.periodSendTelemetry)
-#     while True:
-#         if CLIENT.is_connected():
-#             fake_telemetry = generate_fake_telemetry()
-#             base = fake_telemetry.items()
-#             for key, value in base:
-#                 CLIENT.gw_send_telemetry(key, value)
-#             LOGGER.info('Sent telemetry data')
-#             telemetries_lock.acquire()
-#             telemetries.clear()
-#             telemetries_lock.release()
-#         time.sleep(period)
-
-
 def call():
     period = shared_attributes.get('periodSendTelemetry', default_data.periodSendTelemetry)
     while True:
         if CLIENT.is_connected():
-            real_telemetry = format_telemetry()
-            base = real_telemetry.items()
-            for key, value in base:
+            telemetry = format_telemetry()
+            for key, value in telemetry.items():
                 CLIENT.gw_send_telemetry(key, value)
             LOGGER.info('Sent telemetry data')
             telemetries_lock.acquire()
@@ -33,171 +17,102 @@ def call():
         time.sleep(period)
 
 
-def format_telemetry():
-    transform_telemetry = {
-        "device_misc": [
-            {
-                "miscTemp": telemetries['miscTemp'] if 'miscTemp' in telemetries else 0,
-                "miscHumid": telemetries['miscHumid'] if 'miscHumid' in telemetries else 0
-            }
-        ],
-        "device_airc": [
-            {
-                "aircTemp": telemetries['aircTemp'] if 'aircTemp' in telemetries else 0,
-                "aircHumid": telemetries['aircHumid'] if 'aircHumid' in telemetries else 0,
-                "aircAirc1Temp": telemetries['aircAirc1Temp'] if 'aircAirc1Temp' in telemetries else 0,
-                "aircAirc2Temp": telemetries['aircAirc2Temp'] if 'aircAirc2Temp' in telemetries else 0,
-                "aircOutdoorTemp": telemetries['aircOutdoorTemp'] if 'aircOutdoorTemp' in telemetries else 0
-            }
-        ],
-        "device_ats": [
-            {
-                "atsVacP1": telemetries['atsVacP1'] if 'atsVacP1' in telemetries else 0,
-                "atsVacP2": telemetries['atsVacP2'] if 'atsVacP2' in telemetries else 0,
-                "atsVacP3": telemetries['atsVacP3'] if 'atsVacP3' in telemetries else 0,
-                "atsVacFreq": telemetries['atsVacFreq'] if 'atsVacFreq' in telemetries else 0,
-                "atsVgenP1": telemetries['atsVgenP1'] if 'atsVgenP1' in telemetries else 0,
-                "atsVgenP2": telemetries['atsVgenP2'] if 'atsVgenP2' in telemetries else 0,
-                "atsVgenP3": telemetries['atsVgenP3'] if 'atsVgenP3' in telemetries else 0,
-                "atsVgenFreq": telemetries['atsVgenFreq'] if 'atsVgenFreq' in telemetries else 0,
-                "atsVloadP1": telemetries['atsVloadP1'] if 'atsVloadP1' in telemetries else 0,
-                "atsVloadP2": telemetries['atsVloadP2'] if 'atsVloadP2' in telemetries else 0,
-                "atsVloadP3": telemetries['atsVloadP3'] if 'atsVloadP3' in telemetries else 0,
-                "atsVloadFreq": telemetries['atsVloadFreq'] if 'atsVloadFreq' in telemetries else 0,
-                "atsIloadP1": telemetries['atsIloadP1'] if 'atsIloadP1' in telemetries else 0,
-                "atsIloadP2": telemetries['atsIloadP2'] if 'atsIloadP2' in telemetries else 0,
-                "atsIloadP3": telemetries['atsIloadP3'] if 'atsIloadP3' in telemetries else 0,
-                "atsGscOil": telemetries['atsGscOil'] if 'atsGscOil' in telemetries else 0,
-                "atsGscCoolantTemp": telemetries['atsGscCoolantTemp'] if 'atsGscCoolantTemp' in telemetries else 0,
-                "atsGscFuel": telemetries['atsGscFuel'] if 'atsGscFuel' in telemetries else 0,
-                "atsGscVbat": telemetries['atsGscVbat'] if 'atsGscVbat' in telemetries else 0,
-                "atsGscSpeed": telemetries['atsGscSpeed'] if 'atsGscSpeed' in telemetries else 0,
-                "atsGscPowerTotal": telemetries['atsGscPowerTotal'] if 'atsGscPowerTotal' in telemetries else 0,
-                "atsGscPower1": telemetries['atsGscPower1'] if 'atsGscPower1' in telemetries else 0,
-                "atsGscPower2": telemetries['atsGscPower2'] if 'atsGscPower2' in telemetries else 0,
-                "atsGscPower3": telemetries['atsGscPower3'] if 'atsGscPower3' in telemetries else 0,
-                "atsGscKvaTotal": telemetries['atsGscKvaTotal'] if 'atsGscKvaTotal' in telemetries else 0,
-                "atsGscKva1": telemetries['atsGscKva1'] if 'atsGscKva1' in telemetries else 0,
-                "atsGscKva2": telemetries['atsGscKva2'] if 'atsGscKva2' in telemetries else 0,
-                "atsGscKva3": telemetries['atsGscKva3'] if 'atsGscKva3' in telemetries else 0,
-                "atsGscRunHoursCounter": telemetries['atsGscRunHoursCounter'] if 'atsGscRunHoursCounter' in telemetries else 0,
-                "atsGscCrankCounter": telemetries['atsGscCrankCounter'] if 'atsGscCrankCounter' in telemetries else 0
-            }
-        ],
-        "device_atu": [
-            {
-                "atuAtu1X": telemetries['atuAtu1X']  if 'atuAtu1X' in telemetries else 0,
-                "atuAtu1Y": telemetries['atuAtu1Y']  if 'atuAtu1Y' in telemetries else 0,
-                "atuAtu1Z": telemetries['atuAtu1Z']  if 'atuAtu1Z' in telemetries else 0,
-                "atuAtu2X": telemetries['atuAtu2X']  if 'atuAtu2X' in telemetries else 0,
-                "atuAtu2Y": telemetries['atuAtu2Y']  if 'atuAtu2Y' in telemetries else 0,
-                "atuAtu2Z": telemetries['atuAtu2Z']  if 'atuAtu2Z' in telemetries else 0,
-                "atuAtu3X": telemetries['atuAtu3X']  if 'atuAtu3X' in telemetries else 0,
-                "atuAtu3Y": telemetries['atuAtu3Y']  if 'atuAtu3Y' in telemetries else 0,
-                "atuAtu3Z": telemetries['atuAtu3Z']  if 'atuAtu3Z' in telemetries else 0
-            }
-        ],
-        "device_dc": [
-            {
-                "dcVdc": telemetries['dcVdc'] if 'dcVdc' in telemetries else 0,
-                "dcIbat1": telemetries['dcIbat1'] if 'dcIbat1' in telemetries else 0,
-                "dcBat1Temp": telemetries['dcBat1Temp'] if 'dcBat1Temp' in telemetries else 0,
-                "dcVbat1Div2": telemetries['dcVbat1Div2'] if 'dcVbat1Div2' in telemetries else 0,
-                "dcIbat2": telemetries['dcIbat2'] if 'dcIbat2' in telemetries else 0,
-                "dcBat2Temp": telemetries['dcBat2Temp'] if 'dcBat2Temp' in telemetries else 0,
-                "dcVbat2Div2": telemetries['dcVbat2Div2'] if 'dcVbat2Div2' in telemetries else 0,
-                "dcIbat3": telemetries['dcIbat3'] if 'dcIbat3' in telemetries else 0,
-                "dcBat3Temp": telemetries['dcBat3Temp'] if 'dcBat3Temp' in telemetries else 0,
-                "dcVbat3Div2": telemetries['dcVbat3Div2'] if 'dcVbat3Div2' in telemetries else 0,
-                "dcIbat4": telemetries['dcIbat4'] if 'dcIbat4' in telemetries else 0,
-                "dcBat4Temp": telemetries['dcBat4Temp'] if 'dcBat4Temp' in telemetries else 0,
-                "dcVbat4Div2": telemetries['dcVbat4Div2'] if 'dcVbat4Div2' in telemetries else 0
-            }
-        ]
-    }
-    return transform_telemetry
-
-
-def generate_fake_telemetry():
+def replica_telemetry():
     telemetry = {
-        "device_misc": [
-            {
-                "miscTemp": 19,
-                "miscHumid": 60
-            }
-        ],
-        "device_airc": [
-            {
-                "aircTemp": 35,
-                "aircHumid": 85,
-                "aircAirc1Temp": 55,
-                "aircAirc2Temp": 52,
-                "aircOutdoorTemp": 36
-            }
-        ],
-        "device_ats": [
-            {
-                "atsVacP1": 220,
-                "atsVacP2": 10,
-                "atsVacP3": 11,
-                "atsVacFreq": 13,
-                "atsVgenP1": 10,
-                "atsVgenP2": 11,
-                "atsVgenP3": 12,
-                "atsVgenFreq": 17,
-                "atsVloadP1": 20,
-                "atsVloadP2": 21,
-                "atsVloadP3": 22,
-                "atsVloadFreq": 23,
-                "atsIloadP1": 24,
-                "atsIloadP2": 21,
-                "atsIloadP3": 22,
-                "atsGscOil": 11,
-                "atsGscCoolantTemp": 27,
-                "atsGscFuel": 31,
-                "atsGscVbat": 34,
-                "atsGscSpeed": 32,
-                "atsGscPowerTotal": 125,
-                "atsGscPower1": 45,
-                "atsGscPower2": 40,
-                "atsGscPower3": 40,
-                "atsGscKvaTotal": 40,
-                "atsGscKva1": 10,
-                "atsGscKva2": 20,
-                "atsGscKva3": 10,
-                "atsGscRunHoursCounter": 60,
-                "atsGscCrankCounter": 10
-            }
-        ],
-        "device_atu": [
-            {
-                "atuAtu1X": 10,
-                "atuAtu1Y": 20,
-                "atuAtu1Z": 30,
-                "atuAtu2X": 10,
-                "atuAtu2Y": 20,
-                "atuAtu2Z": 30,
-                "atuAtu3X": 10,
-                "atuAtu3Y": 20,
-                "atuAtu3Z": 30
-            }
-        ],
-        "device_dc": [
-            {
-                "dcVdc": 21,
-                "dcIbat1": 22,
-                "dcBat1Temp": 23,
-                "dcVbat1Div2": 30,
-                "dcIbat2": 12,
-                "dcBat2Temp": 13,
-                "dcVbat2Div2": 14,
-                "dcIbat3": 15,
-                "dcBat3Temp": 16,
-                "dcVbat3Div2": 17,
-                "dcIbat4": 18,
-                "dcBat4Temp": 19,
-                "dcVbat4Div2": 20
-            }
-        ]
+        "miscTemp": 190,
+        "miscHumid": 600,
+        "aircTemp": 35,
+        "aircHumid": 85,
+        "aircAirc1Temp": 30,
+        "aircAirc2Temp": 26,
+        "aircOutdoorTemp": 36,
+        "atsVacP1": 220,
+        "atsVacP2": 10,
+        "atsVacP3": 11,
+        "atsVacFreq": 13,
+        "atsVgenP1": 10,
+        "atsVgenP2": 11,
+        "atsVgenP3": 12,
+        "atsVgenFreq": 17,
+        "atsVloadP1": 20,
+        "atsVloadP2": 21,
+        "atsVloadP3": 22,
+        "atsVloadFreq": 23,
+        "atsIloadP1": 24,
+        "atsIloadP2": 21,
+        "atsIloadP3": 22,
+        "atsGscOil": 11,
+        "atsGscCoolantTemp": 27,
+        "atsGscFuel": 31,
+        "atsGscVbat": 34,
+        "atsGscSpeed": 32,
+        "atsGscPowerTotal": 125,
+        "atsGscPower1": 45,
+        "atsGscPower2": 40,
+        "atsGscPower3": 40,
+        "atsGscKvaTotal": 40,
+        "atsGscKva1": 10,
+        "atsGscKva2": 20,
+        "atsGscKva3": 10,
+        "atsGscRunHoursCounter": 60,
+        "atsGscCrankCounter": 10,
+        "atuAtu1X": 10,
+        "atuAtu1Y": 20,
+        "atuAtu1Z": 30,
+        "atuAtu2X": 10,
+        "atuAtu2Y": 20,
+        "atuAtu2Z": 30,
+        "atuAtu3X": 10,
+        "atuAtu3Y": 20,
+        "atuAtu3Z": 30,
+        "dcVdc": 21,
+        "dcIbat1": 22,
+        "dcBat1Temp": 23,
+        "dcVbat1Div2": 30,
+        "dcIbat2": 12,
+        "dcBat2Temp": 13,
+        "dcVbat2Div2": 14,
+        "dcIbat3": 15,
+        "dcBat3Temp": 16,
+        "dcVbat3Div2": 17,
+        "dcIbat4": 18,
+        "dcBat4Temp": 19,
+        "dcVbat4Div2": 20
     }
+
     return telemetry
+
+
+def format_telemetry():
+    list_telemetry = {'device_misc': [{}], 'device_airc': [{}], 'device_ats': [{}], 'device_atu': [{}],
+                      'device_dc': [{}]}
+    telemetry_misc = {}
+    telemetry_airc = {}
+    telemetry_ats = {}
+    telemetry_atu = {}
+    telemetry_dc = {}
+    data_from_stm32 = replica_telemetry()
+    for key, value in data_from_stm32.items():
+        if 'misc' in key:
+            telemetry_misc[key] = value
+        elif 'airc' in key:
+            telemetry_airc[key] = value
+        elif 'ats' in key:
+            telemetry_ats[key] = value
+        elif 'atu' in key:
+            telemetry_atu[key] = value
+        elif 'dc' in key:
+            telemetry_dc[key] = value
+
+    if telemetry_misc:
+        list_telemetry['device_misc'] = [telemetry_misc]
+    if telemetry_airc:
+        list_telemetry['device_airc'] = [telemetry_airc]
+    if telemetry_ats:
+        list_telemetry['device_ats'] = [telemetry_ats]
+    if telemetry_atu:
+        list_telemetry['device_atu'] = [telemetry_atu]
+    if telemetry_dc:
+        list_telemetry['device_dc'] = [telemetry_dc]
+
+    return list_telemetry
--- operate/update_attributes_thread.py
+++ operate/update_attributes_thread.py
@@ -3,35 +3,14 @@ import time
 from config import *
 
 
-# def call():
-#     period = shared_attributes.get('periodUpdate', default_data.periodUpdate)
-#     while True:
-#         if CLIENT.is_connected():
-#             update_attributes_lock.acquire()
-#             update_attributes = fake_client_attributes()
-#             if update_attributes:
-#                 base = update_attributes.items()
-#                 for key, value in base:
-#                     CLIENT.gw_send_attributes(key, value)
-#                 LOGGER.info('Sent changed client attributes')
-#                 log_info = []
-#                 for key, value in update_attributes.items():
-#                     log_info.append('\t{:>20s}: {:>20s}'.format(str(key), str(value)))
-#                 LOGGER.info('\n'.join(log_info))
-#                 update_attributes.clear()
-#             update_attributes_lock.release()
-#         time.sleep(period)
-
-
 def call():
     period = shared_attributes.get('periodUpdate', default_data.periodUpdate)
     while True:
         if CLIENT.is_connected():
             update_attributes_lock.acquire()
-            real_client_attributes = format_client_attributes()
-            if real_client_attributes:
-                base = real_client_attributes.items()
-                for key, value in base:
+            gw_client_attributes = format_client_attributes()
+            if gw_client_attributes:
+                for key, value in gw_client_attributes.items():
                     CLIENT.gw_send_attributes(key, value)
                 LOGGER.info('Sent changed client attributes')
                 log_info = []
@@ -43,129 +22,113 @@ def call():
         time.sleep(period)
 
 
+def replica_client_attributes():
+    attributes = {
+        "aircAirc2RunState": 0,
+        "aircAirc1RunState": 0,
+        "aircOnlineStatus": 1,
+        "aircAirc1Command": 1,
+        "aircAirc1RunThreshold": 26,
+        "aircLearnCmdId": 0,
+        "aircAirc2Command": 1,
+        "aircAirc1Error": 0,
+        "aircAirc2RunThreshold": 26,
+        "aircAirc2Error": 0,
+        "aircIrStatus": 1,
+        "atsBatFull": 1,
+        "atsGscType": 1,
+        "atsIsAllBatFull": 0,
+        "atsGscPwrCounter": 1,
+        "atsGscAlarm": 0,
+        "atsMode": 0,
+        "atsContactorState": 1,
+        "atsCommState": 1,
+        "atsOnlineStatus": 1,
+        "atsType": 1,
+        "atuOnlineStatus": 1,
+        "crmuCardId": "AAA",
+        "crmuOnlineStatus": 1,
+        "crmuDoorState": 1,
+        "miscBellState": 0,
+        "miscFanState": 0,
+        "miscVbus": 1,
+        "miscV12": 1,
+        "miscDin3": 0,
+        "miscDin2": 0,
+        "miscDin1": 0,
+        "miscDin0": 0,
+        "miscDin7": 0,
+        "miscDin6": 0,
+        "miscDin5": 0,
+        "miscDin4": 0,
+        "dcOnlineStatus": 22,
+        "dcRectState": 111,
+        "moveSensor": 1,
+        "smokeState": 1,
+        "fireState": 1,
+        "offOnFire": 1,
+        "floodState": 1
+    }
+
+    return attributes
+
+
 def format_client_attributes():
-    transform_client_attributes = {
-        "device_misc": {
-            "miscDin0": update_attributes['miscDin0'],
-            "miscDin1": update_attributes['miscDin1'],
-            "miscDin2": update_attributes['miscDin2'],
-            "miscDin3": update_attributes['miscDin3'],
-            "miscDin4": update_attributes['miscDin4'],
-            "miscDin5": update_attributes['miscDin5'],
-            "miscDin6": update_attributes['miscDin6'],
-            "miscDin7": update_attributes['miscDin7'],
-            "miscFanState": update_attributes['miscFanState'],
-            "miscBellState": update_attributes['miscBellState'],
-            "miscV12": update_attributes['miscV12'],
-            "miscVbus": update_attributes['miscVbus']
-        },
-        "device_airc": {
-            "aircOnlineStatus": update_attributes['aircOnlineStatus'],
-            "aircAirc1RunState": update_attributes['aircAirc1RunState'],
-            "aircAirc1Error": update_attributes['aircAirc1Error'],
-            "aircAirc2RunState": update_attributes['aircAirc2RunState'],
-            "aircAirc2Error": update_attributes['aircAirc2Error'],
-            "aircAirc1RunThreshold": update_attributes['aircAirc1RunThreshold'],
-            "aircAirc2RunThreshold": update_attributes['aircAirc2RunThreshold'],
-            "aircAirc1Command": update_attributes['aircAirc1Command'],
-            "aircAirc2Command": update_attributes['aircAirc2Command'],
-            "aircIrStatus": update_attributes['aircIrStatus'],
-            "aircLearnCmdId": update_attributes['aircLearnCmdId']
-        },
-        "device_ats": {
-            "atsOnlineStatus": update_attributes['atsOnlineStatus'],
-            "atsCommState": update_attributes['atsCommState'],
-            "atsMode": update_attributes['atsMode'],
-            "atsType": update_attributes['atsType'],
-            "atsContactorState": update_attributes['atsContactorState'],
-            "atsBatFull": update_attributes['atsBatFull'],
-            "atsIsAllBatFull": update_attributes['atsIsAllBatFull'],
-            "atsGscType": update_attributes['atsGscType'],
-            "atsGscPwrCounter": update_attributes['atsGscPwrCounter'],
-            "atsGscAlarm": update_attributes['atsGscAlarm']
-        },
-        "device_atu": {
-            "atuOnlineStatus": update_attributes['atuOnlineStatus']
-        },
-        "device_dc": {
-            "dcOnlineStatus": update_attributes['dcOnlineStatus'],
-            "dcRectState": update_attributes['dcRectState']
-        },
-        "device_crmu": {
-            "crmuOnlineStatus": update_attributes['crmuOnlineStatus'],
-            "crmuCardId": update_attributes['crmuCardId'],
-            "crmuDoorState": update_attributes['crmuDoorState']
-        },
-        "gateway_python_001": {
-            "fireState": update_attributes['fireState'],
-            "offOnFire": update_attributes['offOnFire'],
-            "smokeState": update_attributes['smokeState'],
-            "floodState": update_attributes['floodState'],
-            "moveSensor": update_attributes['moveSensor']
-        }
-    }
-    return transform_client_attributes
+    list_client_attributes = {'device_misc': {}, 'device_airc': {}, 'device_ats': {}, 'device_atu': {},
+                              'device_dc': {}, 'device_crmu': {}, 'device_fire_sensor': {}, 'device_smoke_sensor': {},
+                              'device_move_sensor': {}, 'device_flood_sensor': {}}
+    client_attributes_misc = {}
+    client_attributes_airc = {}
+    client_attributes_ats = {}
+    client_attributes_atu = {}
+    client_attributes_dc = {}
+    client_attributes_crmu = {}
+    client_attributes_fire_sensor = {}
+    client_attributes_smoke_sensor = {}
+    client_attributes_move_sensor = {}
+    client_attributes_flood_sensor = {}
+    data_from_stm32 = replica_client_attributes()
+    for key, value in data_from_stm32.items():
+        if 'misc' in key:
+            client_attributes_misc[key] = value
+        elif 'airc' in key:
+            client_attributes_airc[key] = value
+        elif 'ats' in key:
+            client_attributes_ats[key] = value
+        elif 'atu' in key:
+            client_attributes_atu[key] = value
+        elif 'dc' in key:
+            client_attributes_dc[key] = value
+        elif 'crmu' in key:
+            client_attributes_crmu[key] = value
+        elif 'fireState' in key:
+            client_attributes_fire_sensor[key] = value
+        elif 'smokeState' in key:
+            client_attributes_smoke_sensor[key] = value
+        elif 'moveSensor' in key:
+            client_attributes_move_sensor[key] = value
+        elif 'floodState' in key:
+            client_attributes_flood_sensor[key] = value
 
+    if client_attributes_misc:
+        list_client_attributes['device_misc'] = client_attributes_misc
+    if client_attributes_airc:
+        list_client_attributes['device_airc'] = client_attributes_airc
+    if client_attributes_ats:
+        list_client_attributes['device_ats'] = client_attributes_ats
+    if client_attributes_atu:
+        list_client_attributes['device_atu'] = client_attributes_atu
+    if client_attributes_dc:
+        list_client_attributes['device_dc'] = client_attributes_dc
+    if client_attributes_crmu:
+        list_client_attributes['device_crmu'] = client_attributes_crmu
+    if client_attributes_fire_sensor:
+        list_client_attributes['device_fire_sensor'] = client_attributes_fire_sensor
+    if client_attributes_smoke_sensor:
+        list_client_attributes['device_smoke_sensor'] = client_attributes_smoke_sensor
+    if client_attributes_move_sensor:
+        list_client_attributes['device_move_sensor'] = client_attributes_move_sensor
+    if client_attributes_flood_sensor:
+        list_client_attributes['device_flood_sensor'] = client_attributes_flood_sensor
 
-def fake_client_attributes():
-    client_attributes = {
-        "device_misc": {
-            "miscDin0": 1,
-            "miscDin1": 1,
-            "miscDin2": 1,
-            "miscDin3": 1,
-            "miscDin4": 1,
-            "miscDin5": 1,
-            "miscDin6": 1,
-            "miscDin7": 1,
-            "miscFanState": 1,
-            "miscBellState": 1,
-            "miscV12": 1,
-            "miscVbus": 1
-        },
-        "device_airc": {
-            "aircOnlineStatus": 1,
-            "aircAirc1RunState": 0,
-            "aircAirc1Error": 0,
-            "aircAirc2RunState": 0,
-            "aircAirc2Error": 0,
-            "aircAirc1RunThreshold": 26,
-            "aircAirc2RunThreshold": 26,
-            "aircAirc1Command": 1,
-            "aircAirc2Command": 1,
-            "aircIrStatus": 1,
-            "aircLearnCmdId": 0
-        },
-        "device_ats": {
-            "atsOnlineStatus": 1,
-            "atsCommState": 1,
-            "atsMode": 0,
-            "atsType": 1,
-            "atsContactorState": 1,
-            "atsBatFull": 1,
-            "atsIsAllBatFull": 0,
-            "atsGscType": 1,
-            "atsGscPwrCounter": 1,
-            "atsGscAlarm": 0
-        },
-        "device_atu": {
-            "atuOnlineStatus": 1
-        },
-        "device_dc": {
-            "dcOnlineStatus": 1,
-            "dcRectState": 1
-        },
-        "device_crmu": {
-            "crmuOnlineStatus": 1,
-            "crmuCardId": "AAA",
-            "crmuDoorState": 1
-        },
-        "gateway_python_001": {
-            "fireState": 0,
-            "offOnFire": 0,
-            "smokeState": 0,
-            "floodState": 0,
-            "moveSensor": 0
-        }
-    }
-    return client_attributes
+    return list_client_attributes
