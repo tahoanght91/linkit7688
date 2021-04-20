279e77c9ebf5123866648543f42af122fab7ac08
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-20 09:30:43 +0700

    add new constants file

---
+++ config/common.py
@@ -0,0 +1,52 @@
+#device_name
+DEVICE_AIRC = 'device_airc'
+DEVICE_AIRC_1 = 'device_airc1'
+DEVICE_AIRC_2 = 'device_airc2'
+DEVICE_MISC = 'device_misc'
+DEVICE_CRMU = 'device_crmu'
+DEVICE_ATU = 'device_atu'
+DEVICE_DC = 'device_dc'
+DEVICE_ATS = 'device_ats'
+DEVICE_FIRE_SENSOR = 'device_fire_sensor'
+DEVICE_FLOOD_SENSOR = 'device_flood_sensor'
+DEVICE_MOVE_SENSOR = 'device_move_sensor'
+DEVICE_SMOKE_SENSOR = 'device_smoke_sensor'
+
+#rpc
+AUTO = 'setAuto'
+CONTROL = 'setControl'
+GET_STATE = 'getState'
+
+#method
+#airc1
+GET_AIRC_CONTROL_1 = 'getStateControlAirc1'
+SET_AIRC_CONTROL_1 = 'setControlAirc1'
+GET_AIRC_AUTO_1 = 'getStateAutoAirc1'
+SET_AIRC_AUTO_1 = 'setAutoAirc1'
+#airc2
+GET_AIRC_CONTROL_2 = 'getStateControlAirc2'
+SET_AIRC_CONTROL_2 = 'setControlAirc2'
+GET_AIRC_AUTO_2 = 'getStateAutoAirc2'
+SET_AIRC_AUTO_2 = 'setAutoAirc2'
+#fan
+GET_FAN_CONTROL = 'getStateControlFan'
+SET_FAN_CONTROL = 'setControlFan'
+GET_FAN_AUTO = 'getStateAutoFan'
+SET_FAN_AUTO = 'setAutoFan'
+#ats
+# GET_ATS_CONTROL = 'getStateControlAts'
+# SET_ATS_CONTROL = 'setControlAts'
+# GET_ATS_AUTO = 'getStateAutoAts'
+# SET_ATS_AUTO = 'setAutoAts'
+
+GET_ATS_CONTROL_MAIN = 'getStateControlAtsMain'
+SET_ATS_CONTROL_MAIN = 'setControlAtsMain'
+GET_ATS_CONTROL_GEN = 'getStateControlAtsGen'
+SET_ATS_CONTROL_GEN = 'setControlAtsGen'
+GET_ATS_AUTO = 'getStateAutoAts'
+SET_ATS_AUTO = 'setAutoAts'
+#crmu
+GET_CRMU_CONTROL = 'getStateControlCrmu'
+SET_CRMU_CONTROL = 'setControlCrmu'
+GET_CRMU_AUTO = 'getStateAutoCrmu'
+SET_CRMU_AUTO = 'setAutoCrmu'
