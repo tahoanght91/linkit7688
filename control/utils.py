5ce8fbfd9e63399932a28af2beae271b13f600f1
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-20 09:39:57 +0700

    edit execute command (send command to stm32)

--- control/utils.py
+++ control/utils.py
@@ -1,67 +1,123 @@
 import struct
 
 from config import *
-from utility import with_check_sum
+from config.common import *
+
 
 def _check_command(device, command):
-    return (device == 'fan' and (command == 'off' or command == 'on') and not shared_attributes.get('miscFanControlAuto', default_data.miscFanControlAuto)
-            or device == 'airc1' and (command == 'off' or command == 'on') and not shared_attributes.get('aircControlAuto', default_data.aircControlAuto)
-            or device == 'airc2' and (command == 'off' or command == 'on') and not shared_attributes.get('aircControlAuto', default_data.aircControlAuto)
-            or device == 'ats' and (command == 'main' or command == 'gen' or command == 'test') and not shared_attributes.get('atsControlAuto', default_data.atsControlAuto)
-            or device == 'crmu' and (command == 'off' or command == 'on') and not shared_attributes.get('crmuControlAuto', default_data.crmuControlAuto))
+    if device == DEVICE_AIRC and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('aircControlAuto', default_data.aircControlAuto):
+        return True
+    elif device == DEVICE_MISC and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('miscFanControlAuto', default_data.miscFanControlAuto):
+        return True
+    elif device == DEVICE_ATS and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('atsControlAuto', default_data.atsControlAuto):
+        return True
+    elif device == DEVICE_CRMU and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('crmuControlAuto', default_data.crmuControlAuto):
+        return True
+    else:
+        return False
 
 
 def _process_set_auto(device, command):
-    if not (type(command) == bool and (device == 'fan' or device == 'airc' or device == 'ats' or device == 'crmu')):
+    if not (type(command) == bool
+            and
+            (device == DEVICE_MISC
+             or device == DEVICE_AIRC
+             or device == DEVICE_ATS
+             or device == DEVICE_CRMU)):
         return False
-    if device == 'airc':
-        shared_attributes['aircControlAuto'] = command
-    elif device == 'fan':
-        shared_attributes['miscFanControlAuto'] = command
-    elif device == 'ats':
-        shared_attributes['atsControlAuto'] = command 
-    else:
-        shared_attributes['crmuControlAuto'] = command
+
+    value = convert_boolean_to_int(command)
+
+    if device == DEVICE_AIRC:
+        shared_attributes['aircControlAuto'] = value
+    elif device == DEVICE_MISC:
+        shared_attributes['miscFanControlAuto'] = value
+    elif device == DEVICE_ATS:
+        shared_attributes['atsControlAuto'] = value
+    elif device == DEVICE_CRMU:
+        shared_attributes['crmuControlAuto'] = value
     return True
 
+
 def _process_command(device, command):
+    value = ''
+
+    if type(command) is bool:
+        value = convert_boolean_to_string(command)
+    else:
+        value = command
+
     if device == 'bell':
         device = 1
         if command == 'off':
             command = 0
         else:
             command = 1
-    elif device == 'fan':
+    elif device == DEVICE_MISC:
         device = 2
-        if command == 'off':
+        if value == 'off':
             command = 0
         else:
             command = 1
-    elif device == 'airc1':
+    elif device == DEVICE_AIRC_1:
         device = 3
-        if command == 'off':
+        if value == 'off':
             command = 0
-        else:
+        elif value == 'on':
             command = 1
-    elif device == 'airc2':
+    elif device == DEVICE_AIRC_2:
         device = 4
-        if command == 'off':
+        if value == 'off':
             command = 0
-        else:
+        elif value == 'on':
             command = 1
-    elif device == 'ats':
+    elif device == DEVICE_ATS:
         device = 5
-        if command == 'main':
+        if value == 'main':
             command = 0
-        elif command == 'gen':
+        elif value == 'gen':
             command = 1
-        else:
+        elif value == 'auto':
             command = 2
-    elif device == 'crmu':
+    elif device == DEVICE_CRMU:
         device = 6
-        if command == 'off':
+        if value == 'off':
             command = 0
         else:
             command = 1
+    LOGGER.debug('Process command: device: %s, command: %s', device, command)
     result = struct.pack('BBBBB', 0xA0, 0x03, 0x21, device, command)
     return result
+
+
+def convert_boolean_to_string(command):
+    if command:
+        _command = 'on'
+    else:
+        _command = 'off'
+
+    return _command
+
+
+def convert_boolean_to_int(command):
+    if command:
+        _command = 1
+    else:
+        _command = 0
+
+    return _command
+
+
+def _check_command_send_rpc(device, command):
+    if device == DEVICE_AIRC_1 and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('aircControlAuto', default_data.aircControlAuto):
+        return True
+    elif device == DEVICE_AIRC_2 and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('miscFanControlAuto', default_data.miscFanControlAuto):
+        return True
+    elif device == DEVICE_MISC and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('miscFanControlAuto', default_data.miscFanControlAuto):
+        return True
+    elif device == DEVICE_ATS and (command == 'main' or command == 'gen' or command == GET_STATE) and not shared_attributes.get('atsControlAuto', default_data.atsControlAuto):
+        return True
+    elif device == DEVICE_CRMU and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('crmuControlAuto', default_data.crmuControlAuto):
+        return True
+    else:
+        return False
