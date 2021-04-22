03fbaaff062d4bc1aaf0cc8c4a6bf6973ee2f95b
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-22 09:18:39 +0700

    edit function _check_command and add new check_state_device function

--- control/utils.py
+++ control/utils.py
@@ -5,16 +5,56 @@ from config.common import *
 
 
 def _check_command(device, command):
-    if device == DEVICE_AIRC and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('aircControlAuto', default_data.aircControlAuto):
+    if device == DEVICE_AIRC_1 and (command == GET_STATE or command == GET_VALUE):
         return True
-    elif device == DEVICE_MISC and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('miscFanControlAuto', default_data.miscFanControlAuto):
+    elif device == DEVICE_AIRC_2 and (command == GET_STATE or command == GET_VALUE):
         return True
-    elif device == DEVICE_ATS and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('atsControlAuto', default_data.atsControlAuto):
+    elif device == DEVICE_MISC and (command == GET_STATE or command == GET_VALUE):
         return True
-    elif device == DEVICE_CRMU and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('crmuControlAuto', default_data.crmuControlAuto):
+    elif device == DEVICE_ATS and (command == GET_STATE or command == GET_VALUE):
+        return True
+    elif device == DEVICE_CRMU and (command == GET_STATE or command == GET_VALUE):
         return True
     else:
         return False
+
+
+def check_state_device(device_name, method):
+    value = ''
+    if device_name == DEVICE_AIRC_1 and method == GET_SATE_AIRC_1:
+        # TODO: change client attributes of airc1:
+        value = client_attributes.get('aircIrStatus', default_data.aircIrStatus)
+    elif device_name == DEVICE_AIRC_2 and method == GET_STATE_AIRC_2:
+        # TODO: change client attributes of airc2:
+        value = client_attributes.get('aircIrStatus', default_data.aircIrStatus)
+    elif device_name == DEVICE_MISC and method == GET_STATE_FAN:
+        # TODO: change client attributes of fan (misc):
+        value = client_attributes.get('miscDin0', default_data.miscDin0)
+    elif device_name == DEVICE_ATS and method == GET_STATE_ATS:
+        value = client_attributes.get('atsMode', default_data.atsMode)
+    elif device_name == DEVICE_CRMU and method == GET_STATE_CRMU:
+        # TODO: change client attributes of crmu:
+        value = client_attributes.get('crmuOnlineStatus', default_data.crmuOnlineStatus)
+    return value
+
+
+def get_value_device(device_name, method):
+    value = ''
+    if device_name == DEVICE_AIRC_1 and method == GET_VALUE_AIRC_1:
+        # TODO: change telemetry of airc1:
+        value = telemetries.get('temp_indoor', default_data.temp_indoor)
+    elif device_name == DEVICE_AIRC_2 and method == GET_VALUE_AIRC_2:
+        # TODO: change telemetry of airc2:
+        value = telemetries.get('temp_indoor', default_data.temp_indoor)
+    # elif device_name == DEVICE_MISC and method == GET_VALUE_FAN:
+    #     # TODO: change telemetry of fan (misc):
+    #     value = telemetries.get('miscDin0', default_data.miscDin0)
+    # elif device_name == DEVICE_ATS and method == GET_STATE_ATS:
+    #     value = client_attributes.get('atsMode', default_data.atsMode)
+    # elif device_name == DEVICE_CRMU and method == GET_STATE_CRMU:
+    #     # TODO: change client attributes of crmu:
+    #     value = client_attributes.get('crmuOnlineStatus', default_data.crmuOnlineStatus)
+    return value
 
 
 def _process_set_auto(device, command):
@@ -111,7 +151,7 @@ def convert_boolean_to_int(command):
 def _check_command_send_rpc(device, command):
     if device == DEVICE_AIRC_1 and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('aircControlAuto', default_data.aircControlAuto):
         return True
-    elif device == DEVICE_AIRC_2 and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('miscFanControlAuto', default_data.miscFanControlAuto):
+    elif device == DEVICE_AIRC_2 and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('aircControlAuto', default_data.miscFanControlAuto):
         return True
     elif device == DEVICE_MISC and (command == False or command == True or command == GET_STATE) and not shared_attributes.get('miscFanControlAuto', default_data.miscFanControlAuto):
         return True
