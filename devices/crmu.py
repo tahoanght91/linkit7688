ae6eb43b3b30f9f47862541a09299374457f77e4
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-23 11:28:25 +0700

    back to old struct

--- devices/crmu.py
+++ devices/crmu.py
@@ -3,18 +3,10 @@ from .utils import _read_attribute
 
 
 def extract(byte_data):
-    # card_id = byte_data[0:12].encode('hex')
-    # door_state = bytes_to_int(byte_data[12])
-    # online_status = bytes_to_int(byte_data[13])
-    #
-    # _read_attribute('crmuCardId', card_id)
-    # _read_attribute('crmuDoorState', door_state)
-    # _read_attribute('crmuOnlineStatus', online_status)
-
     card_id = byte_data[0:12].encode('hex')
-    card_len = bytes_to_int(byte_data[13])
-    door_state = bytes_to_int(byte_data[14])
+    door_state = bytes_to_int(byte_data[12])
+    online_status = bytes_to_int(byte_data[13])
 
     _read_attribute('crmuCardId', card_id)
-    _read_attribute('crmuLen', card_len)
     _read_attribute('crmuDoorState', door_state)
+    _read_attribute('crmuOnlineStatus', online_status)
