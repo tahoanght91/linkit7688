fcb72b6345b71da1d123e0386a315e6bd3529994
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-09 11:17:23 +0700

    edit command mcc and acm

--- config/common.py
+++ config/common.py
@@ -36,36 +36,72 @@ COMMAND_ATS_GEN = 'gen'
 
 # MCC
 # module MCC
-COMMAND_MCC_AUTO_ON = 'onAutoMcc'
-COMMAND_MCC_AUTO_OFF = 'offAutoMcc'
 # door
-COMMAND_MCC_OPEN_DOOR = 'openDoor'
 COMMAND_MCC_CLOSE_DOOR = 'closeDoor'
-# bell
-COMMAND_MCC_ON_BELL = 'onBell'
-COMMAND_MCC_OFF_BELL = 'offBell'
+COMMAND_MCC_OPEN_DOOR = 'openDoor'
 # lamp
-COMMAND_MCC_ON_LAMP = 'onLamp'
 COMMAND_MCC_OFF_LAMP = 'offLamp'
-# device error
-COMMAND_MCC_ON_ERROR = 'onError'
-COMMAND_MCC_OFF_ERROR = 'offError'
+COMMAND_MCC_ON_LAMP = 'onLamp'
+# dout reversed 1
+COMMAND_MCC_OFF_DOUT_REVERSED_1 = 'offDoutReversed1'
+COMMAND_MCC_ON_DOUT_REVERSED_1 = 'offDoutReversed1'
+# dout reversed 2
+COMMAND_MCC_OFF_DOUT_REVERSED_2 = 'offDoutReversed2'
+COMMAND_MCC_ON_DOUT_REVERSED_2 = 'offDoutReversed2'
+# dout reversed 3
+COMMAND_MCC_OFF_DOUT_REVERSED_3 = 'offDoutReversed3'
+COMMAND_MCC_ON_DOUT_REVERSED_3 = 'offDoutReversed3'
+# dout reversed 4
+COMMAND_MCC_OFF_DOUT_REVERSED_4 = 'offDoutReversed4'
+COMMAND_MCC_ON_DOUT_REVERSED_4 = 'offDoutReversed4'
+# dout reversed 5
+COMMAND_MCC_OFF_DOUT_REVERSED_5 = 'offDoutReversed5'
+COMMAND_MCC_ON_DOUT_REVERSED_5 = 'offDoutReversed5'
+# dout reversed 6
+COMMAND_MCC_OFF_DOUT_REVERSED_6 = 'offDoutReversed6'
+COMMAND_MCC_ON_DOUT_REVERSED_6 = 'offDoutReversed6'
+# dout reversed 7
+COMMAND_MCC_OFF_DOUT_REVERSED_7 = 'offDoutReversed7'
+COMMAND_MCC_ON_DOUT_REVERSED_7 = 'offDoutReversed7'
+# dout reversed 8
+COMMAND_MCC_OFF_DOUT_REVERSED_8 = 'offDoutReversed8'
+COMMAND_MCC_ON_DOUT_REVERSED_8 = 'offDoutReversed8'
+# dout reversed 9
+COMMAND_MCC_OFF_DOUT_REVERSED_9 = 'offDoutReversed9'
+COMMAND_MCC_ON_DOUT_REVERSED_9 = 'offDoutReversed9'
+# dout reversed 10
+COMMAND_MCC_OFF_DOUT_REVERSED_10 = 'offDoutReversed10'
+COMMAND_MCC_ON_DOUT_REVERSED_10 = 'offDoutReversed10'
+# bell
+COMMAND_MCC_OFF_BELL = 'offBell'
+COMMAND_MCC_ON_BELL = 'onBell'
+# dout reversed 11
+COMMAND_MCC_OFF_DOUT_REVERSED_11 = 'offDoutReversed11'
+COMMAND_MCC_ON_DOUT_REVERSED_11 = 'offDoutReversed11'
+# dout reversed 12
+COMMAND_MCC_OFF_DOUT_REVERSED_12 = 'offDoutReversed12'
+COMMAND_MCC_ON_DOUT_REVERSED_12 = 'offDoutReversed12'
+# dout reversed 13
+COMMAND_MCC_OFF_DOUT_REVERSED_13 = 'offDoutReversed13'
+COMMAND_MCC_ON_DOUT_REVERSED_13 = 'offDoutReversed13'
+
+
 # compare rfid card
 SHARED_ATTRIBUTES_RFID_CARD = 'mccRespCardId'
 
 # ACM
 # module acm
-COMMAND_ACM_AUTO_ON = 'onAutoAcm'
 COMMAND_ACM_AUTO_OFF = 'offAutoAcm'
+COMMAND_ACM_AUTO_ON = 'onAutoAcm'
 # air conditioner 1
-COMMAND_ACM_AIRC_1_ON = 'onAcmAirc1'
 COMMAND_ACM_AIRC_1_OFF = 'offAcmAirc1'
+COMMAND_ACM_AIRC_1_ON = 'onAcmAirc1'
 # air conditioner 2
-COMMAND_ACM_AIRC_2_ON = 'onAcmAirc2'
 COMMAND_ACM_AIRC_2_OFF = 'offAcmAirc2'
+COMMAND_ACM_AIRC_2_ON = 'onAcmAirc2'
 # fan
-COMMAND_ACM_FAN_ON = 'onAcmFan'
 COMMAND_ACM_FAN_OFF = 'offAcmFan'
+COMMAND_ACM_FAN_ON = 'onAcmFan'
 
 # METHOD
 # module ACM
