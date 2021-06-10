922eb6c371c1e4a451f6148d669987c18681a5d8
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-10 14:47:15 +0700

    edit file common_command.py.py: - add new ATS command. - edit name of MCC, ACM.

--- config/common_command.py
+++ config/common_command.py
@@ -1,71 +1,83 @@
 # COMMAND
 # ATS
-COMMAND_ATS_AUTO = 'auto'
-COMMAND_ATS_MAIN = 'main'
-COMMAND_ATS_GEN = 'gen'
+# electricity supply
+COMMAND_ATS_ELECTRICITY_SUPPLY_OFF = 'offElectricitySupplyAts'
+COMMAND_ATS_ELECTRICITY_SUPPLY_ON = 'onElectricitySupplyAts'
+# generator supply
+COMMAND_ATS_GENERATOR_SUPPLY_OFF = 'offGeneratorSupplyAts'
+COMMAND_ATS_GENERATOR_SUPPLY_ON = 'onGeneratorSupplyAts'
+# generator
+COMMAND_ATS_GENERATOR_OFF = 'offGeneratorAts'
+COMMAND_ATS_GENERATOR_ON = 'onGeneratorAts'
+# start
+COMMAND_ATS_START_OFF = 'offStartAts'
+COMMAND_ATS_START_ON = 'onStartAts'
+# self-propelled
+COMMAND_ATS_SELF_PROPELLED_OFF = 'offSelfPropelledAts'
+COMMAND_ATS_SELF_PROPELLED_ON = 'onSelfPropelledAts'
 
 # MCC
 # door
-COMMAND_MCC_CLOSE_DOOR = 'closeDoor'
-COMMAND_MCC_OPEN_DOOR = 'openDoor'
+COMMAND_MCC_CLOSE_DOOR = 'closeDoorMcc'
+COMMAND_MCC_OPEN_DOOR = 'openDoorMcc'
 # lamp
-COMMAND_MCC_OFF_LAMP = 'offLamp'
-COMMAND_MCC_ON_LAMP = 'onLamp'
+COMMAND_MCC_OFF_LAMP = 'offLampMcc'
+COMMAND_MCC_ON_LAMP = 'onLampMcc'
 # dout reversed 1
-COMMAND_MCC_OFF_DOUT_REVERSED_1 = 'offDoutReversed1'
-COMMAND_MCC_ON_DOUT_REVERSED_1 = 'offDoutReversed1'
+COMMAND_MCC_OFF_DOUT_REVERSED_1 = 'offDoutReversed1Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_1 = 'offDoutReversed1Mcc'
 # dout reversed 2
-COMMAND_MCC_OFF_DOUT_REVERSED_2 = 'offDoutReversed2'
-COMMAND_MCC_ON_DOUT_REVERSED_2 = 'offDoutReversed2'
+COMMAND_MCC_OFF_DOUT_REVERSED_2 = 'offDoutReversed2Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_2 = 'offDoutReversed2Mcc'
 # dout reversed 3
-COMMAND_MCC_OFF_DOUT_REVERSED_3 = 'offDoutReversed3'
-COMMAND_MCC_ON_DOUT_REVERSED_3 = 'offDoutReversed3'
+COMMAND_MCC_OFF_DOUT_REVERSED_3 = 'offDoutReversed3Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_3 = 'offDoutReversed3Mcc'
 # dout reversed 4
-COMMAND_MCC_OFF_DOUT_REVERSED_4 = 'offDoutReversed4'
-COMMAND_MCC_ON_DOUT_REVERSED_4 = 'offDoutReversed4'
+COMMAND_MCC_OFF_DOUT_REVERSED_4 = 'offDoutReversed4Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_4 = 'offDoutReversed4Mcc'
 # dout reversed 5
-COMMAND_MCC_OFF_DOUT_REVERSED_5 = 'offDoutReversed5'
-COMMAND_MCC_ON_DOUT_REVERSED_5 = 'offDoutReversed5'
+COMMAND_MCC_OFF_DOUT_REVERSED_5 = 'offDoutReversed5Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_5 = 'offDoutReversed5Mcc'
 # dout reversed 6
-COMMAND_MCC_OFF_DOUT_REVERSED_6 = 'offDoutReversed6'
-COMMAND_MCC_ON_DOUT_REVERSED_6 = 'offDoutReversed6'
+COMMAND_MCC_OFF_DOUT_REVERSED_6 = 'offDoutReversed6Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_6 = 'offDoutReversed6Mcc'
 # dout reversed 7
-COMMAND_MCC_OFF_DOUT_REVERSED_7 = 'offDoutReversed7'
-COMMAND_MCC_ON_DOUT_REVERSED_7 = 'offDoutReversed7'
+COMMAND_MCC_OFF_DOUT_REVERSED_7 = 'offDoutReversed7Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_7 = 'offDoutReversed7Mcc'
 # dout reversed 8
-COMMAND_MCC_OFF_DOUT_REVERSED_8 = 'offDoutReversed8'
-COMMAND_MCC_ON_DOUT_REVERSED_8 = 'offDoutReversed8'
+COMMAND_MCC_OFF_DOUT_REVERSED_8 = 'offDoutReversed8Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_8 = 'offDoutReversed8Mcc'
 # dout reversed 9
-COMMAND_MCC_OFF_DOUT_REVERSED_9 = 'offDoutReversed9'
-COMMAND_MCC_ON_DOUT_REVERSED_9 = 'offDoutReversed9'
+COMMAND_MCC_OFF_DOUT_REVERSED_9 = 'offDoutReversed9Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_9 = 'offDoutReversed9Mcc'
 # dout reversed 10
-COMMAND_MCC_OFF_DOUT_REVERSED_10 = 'offDoutReversed10'
-COMMAND_MCC_ON_DOUT_REVERSED_10 = 'offDoutReversed10'
+COMMAND_MCC_OFF_DOUT_REVERSED_10 = 'offDoutReversed10Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_10 = 'offDoutReversed10Mcc'
 # bell
-COMMAND_MCC_OFF_BELL = 'offBell'
-COMMAND_MCC_ON_BELL = 'onBell'
+COMMAND_MCC_OFF_BELL = 'offBellMcc'
+COMMAND_MCC_ON_BELL = 'onBellMcc'
 # dout reversed 11
-COMMAND_MCC_OFF_DOUT_REVERSED_11 = 'offDoutReversed11'
-COMMAND_MCC_ON_DOUT_REVERSED_11 = 'offDoutReversed11'
+COMMAND_MCC_OFF_DOUT_REVERSED_11 = 'offDoutReversed11Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_11 = 'offDoutReversed11Mcc'
 # dout reversed 12
-COMMAND_MCC_OFF_DOUT_REVERSED_12 = 'offDoutReversed12'
-COMMAND_MCC_ON_DOUT_REVERSED_12 = 'offDoutReversed12'
+COMMAND_MCC_OFF_DOUT_REVERSED_12 = 'offDoutReversed12Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_12 = 'offDoutReversed12Mcc'
 # dout reversed 13
-COMMAND_MCC_OFF_DOUT_REVERSED_13 = 'offDoutReversed13'
-COMMAND_MCC_ON_DOUT_REVERSED_13 = 'offDoutReversed13'
+COMMAND_MCC_OFF_DOUT_REVERSED_13 = 'offDoutReversed13Mcc'
+COMMAND_MCC_ON_DOUT_REVERSED_13 = 'offDoutReversed13Mcc'
 
 # ACM
 COMMAND_ACM_AUTO_OFF = 'offAutoAcm'
 COMMAND_ACM_AUTO_ON = 'onAutoAcm'
 # air conditioner 1
-COMMAND_ACM_AIRC_1_OFF = 'offAcmAirc1'
-COMMAND_ACM_AIRC_1_ON = 'onAcmAirc1'
+COMMAND_ACM_AIRC_1_OFF = 'offAirc1Acm'
+COMMAND_ACM_AIRC_1_ON = 'onAirc1Acm'
 # air conditioner 2
-COMMAND_ACM_AIRC_2_OFF = 'offAcmAirc2'
-COMMAND_ACM_AIRC_2_ON = 'onAcmAirc2'
+COMMAND_ACM_AIRC_2_OFF = 'offAirc2Acm'
+COMMAND_ACM_AIRC_2_ON = 'onAirc2Acm'
 # fan
-COMMAND_ACM_FAN_OFF = 'offAcmFan'
-COMMAND_ACM_FAN_ON = 'onAcmFan'
+COMMAND_ACM_FAN_OFF = 'offFanAcm'
+COMMAND_ACM_FAN_ON = 'onFanAcm'
 # self-propelled
 COMMAND_ACM_SELF_PROPELLED_OFF = 'offSelfPropelledAcm'
 COMMAND_ACM_SELF_PROPELLED_ON = 'onSelfPropelledAcm'
