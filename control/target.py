12963c30ad7b171dc4d45a69021555f921b96b6c
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-11 19:47:50 +0700

    edit target.py: - edit get_target_by_command_mcc function. remove check is string.

--- control/target.py
+++ control/target.py
@@ -3,44 +3,40 @@ from config import LOGGER
 
 def get_target_by_command_mcc(command):
     LOGGER.info('Enter get_target_by_command_mcc function')
-    LOGGER.info('Type of command is: %s', type(command))
     target = -1
     try:
-        is_string = isinstance(command, str)
-        if is_string:
-            LOGGER.info('Command is string!')
-            if 'DoorMcc' in command:
-                target = 0
-            elif 'LampMcc' in command:
-                target = 1
-            elif 'DoutReversed1Mcc' in command:
-                target = 2
-            elif 'DoutReversed2Mcc' in command:
-                target = 3
-            elif 'DoutReversed3Mcc' in command:
-                target = 4
-            elif 'DoutReversed4Mcc' in command:
-                target = 5
-            elif 'DoutReversed5Mcc' in command:
-                target = 6
-            elif 'DoutReversed6Mcc' in command:
-                target = 7
-            elif 'DoutReversed7Mcc' in command:
-                target = 8
-            elif 'DoutReversed8Mcc' in command:
-                target = 9
-            elif 'DoutReversed9Mcc' in command:
-                target = 10
-            elif 'DoutReversed10Mcc' in command:
-                target = 11
-            elif 'BellMcc' in command:
-                target = 12
-            elif 'DoutReversed11Mcc' in command:
-                target = 13
-            elif 'DoutReversed12Mcc' in command:
-                target = 14
-            elif 'DoutReversed13Mcc' in command:
-                target = 15
+        if 'DoorMcc' in command:
+            target = 0
+        elif 'LampMcc' in command:
+            target = 1
+        elif 'DoutReversed1Mcc' in command:
+            target = 2
+        elif 'DoutReversed2Mcc' in command:
+            target = 3
+        elif 'DoutReversed3Mcc' in command:
+            target = 4
+        elif 'DoutReversed4Mcc' in command:
+            target = 5
+        elif 'DoutReversed5Mcc' in command:
+            target = 6
+        elif 'DoutReversed6Mcc' in command:
+            target = 7
+        elif 'DoutReversed7Mcc' in command:
+            target = 8
+        elif 'DoutReversed8Mcc' in command:
+            target = 9
+        elif 'DoutReversed9Mcc' in command:
+            target = 10
+        elif 'DoutReversed10Mcc' in command:
+            target = 11
+        elif 'BellMcc' in command:
+            target = 12
+        elif 'DoutReversed11Mcc' in command:
+            target = 13
+        elif 'DoutReversed12Mcc' in command:
+            target = 14
+        elif 'DoutReversed13Mcc' in command:
+            target = 15
         else:
             LOGGER.error('Command is not a string: %s', str(command))
     except Exception as ex:
