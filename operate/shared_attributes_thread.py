0cd7abdb0489d72f801486af55b3c738ddb4583a
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-14 18:33:00 +0700

    edit file shared_attributes_thread.py: - edit period time

--- operate/shared_attributes_thread.py
+++ operate/shared_attributes_thread.py
@@ -24,7 +24,7 @@ list_dict_mcc = []
 
 def call():
     try:
-        period = 3000
+        period = shared_attributes.get('mccPeriodUpdate', default_data.mccPeriodUpdate)
         while True:
             if CLIENT.is_connected():
                 for key, value in shared_attributes.items():
