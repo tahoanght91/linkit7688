241ed337b8a3d319e0f427e736fdecde4c6f7f36
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-18 20:49:40 +0700

    change max tries 3 to 2

--- config/__init__.py
+++ config/__init__.py
@@ -18,7 +18,8 @@ IO_PORT = '/dev/ttyS0'
 # IO_PORT = 'COM3'
 BAUDRATE = 115200
 UPDATE_PERIOD = 7 * 24 * 60 * 60
-READ_PER_WRITE = 20
+READ_PER_WRITE = 1
+MAX_TRIES = 2
 
 shared_attributes = {}
 client_attributes = {}
