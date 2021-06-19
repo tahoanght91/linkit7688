b4cd9ce985cc56dd634186a8432fdcc6a37d2427
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-19 18:32:01 +0700

    edit color led

--- operate/led_thread.py
+++ operate/led_thread.py
@@ -23,15 +23,15 @@ def get_led_value():
         dct_led[LED_ATS] = client_attributes['atsErrorState']
         dct_led[LED_DC] = client_attributes['mccDcCabinetSate']
         dct_led[LED_ACM] = client_attributes['acmIState']
-        dct_led[LED_ATU] = 1
-        dct_led[LED_1] = 1
-        dct_led[LED_2] = 1
-        dct_led[LED_NONE] = 1
+        dct_led[LED_ATU] = 0
+        dct_led[LED_1] = 0
+        dct_led[LED_2] = 0
+        dct_led[LED_NONE] = 0
         dct_led[LED_ALARM] = get_sate_led_alarm(telemetries, dct_last_trace_led_alarm)   # TODO: need function check
-        dct_led[LED_3G] = 1  # TODO: need function check
-        dct_led[LED_ETHERNET] = 0   # TODO: need function check
+        dct_led[LED_3G] = 0  # TODO: need function check
+        dct_led[LED_ETHERNET] = 1   # TODO: need function check
         dct_led[LED_CRMU] = client_attributes['mccRfidConnectState']
-        dct_led[LED_4] = 1
+        dct_led[LED_4] = 0
     except Exception as ex:
         LOGGER.error('Error at get_led_value function with message: %s', ex.message)
     return dct_led
@@ -87,8 +87,8 @@ def get_sate_led_alarm(dct_telemetry, dct_last_trace):
 def get_state_led_server():
     try:
         if CLIENT.is_connected():
-            return 0
-        else:
             return 1
+        else:
+            return 0
     except Exception as ex:
         LOGGER.error('Error at get_state_led_server function with message: %s', ex.message)
