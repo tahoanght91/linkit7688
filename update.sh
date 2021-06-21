e12d670ff55353a5fa9e5316b49f59fe467e3e62
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-21 21:21:22 +0700

    copy file devices.json b4 update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 /etc/init.d/7688 stop
+cp config/devices.json /IoT
 rm -rf linkit7688
 git clone https://github.com/huyfr/linkit7688.git
 cd linkit7688
 git checkout huyfr
-cp config/devices.json /IoT
