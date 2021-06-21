aa264f3b9c1ca07d4697e86a246b25bf6c20563d
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-21 21:40:50 +0700

    edit script update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ rm -rf linkit7688
 git clone https://github.com/huyfr/linkit7688.git
 cd linkit7688
 git checkout huyfr
-cp IoT/devices.json /IoT/linkit7688/config
+cp /IoT/devices.json /IoT/linkit7688/config/
 /etc/init.d/7688 start
