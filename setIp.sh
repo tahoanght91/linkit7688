62ae996bb212241243e98bfa4d1acd9888080324
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-22 09:50:35 +0700

    update file setIp.sh

--- setIp.sh
+++ setIp.sh
@@ -2,5 +2,7 @@ uci set network.lan.ipaddr='10.6.71.88'
 uci set network.lan.gateway='10.6.71.99'
 uci set network.lan.netmask='255.255.255.0'
 uci set network.lan.proto='static'
+uci add_list network.lan.dns='10.3.12.17'
+uci add_list network.lan.dns='10.3.12.18'
 uci commit
 reboot
