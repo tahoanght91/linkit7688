5294f113f595da44debe8da71ef1f8241a16344a
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-22 19:02:00 +0700

    remove monitor thread

--- operate/main_thread.py
+++ operate/main_thread.py
@@ -96,7 +96,7 @@ def call():
         CLIENT.gw_subscribe_to_all_attributes(callback=subscription_thread._attribute_change_callback)
         CLIENT.gw_set_server_side_rpc_request_handler(handler=subscription_thread._gw_rpc_callback)
 
-        thread_list = [io_thread, update_attributes_thread, telemetry_thread, led_thread, monitor_thread, lcd_thread, shared_attributes_thread, rfid_thread]
+        thread_list = [io_thread, update_attributes_thread, telemetry_thread, led_thread, lcd_thread, shared_attributes_thread, rfid_thread]
 
         # enable when test flow read value
         # thread_list = [io_thread, update_attributes_thread, telemetry_thread]
