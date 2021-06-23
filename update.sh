9a5d706e29674c2d9d90b49e917b0c6de7582f8d
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-23 19:41:22 +0700

    update code update

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ cp /IoT/linkit7688/config/devices.json /IoT
 mv /IoT/linkit7688 /
 curl -LO https://github.com/huyfr/linkit7688/archive/refs/heads/huyfr.zip
 unzip huyfr
-mv /linkit7688-huyfr /IoT/linkit7688
+mv /IoT/linkit7688-huyfr linkit7688
 cp /IoT/devices.json /IoT/linkit7688/config/
 rm -rf /linkit7688
-rm -rf /huyfr.zip
+rm -rf /IoT/huyfr.zip
 /etc/init.d/7688 start
