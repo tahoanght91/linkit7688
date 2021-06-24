511aa4eb167f13595eb6f034d52ba94de09ff7be
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-24 16:33:03 +0700

    release version 1.5: - change float to long time

--- operate/lcd_thread.py
+++ operate/lcd_thread.py
@@ -151,7 +151,7 @@ def navigate_lcd_service(key_code):
 def write_body_send_shared_attributes(key, value):
     body = {}
     try:
-        now = round(time.time() * 1000)
+        now = long(time.time() * 1000)
         body = {"tramEntityId": str(device_config['device_id']), "value": str(value), "keyName": str(key), "changeAt": now}
         LOGGER.info('Content of body send shared attributes to Smartsite: %s', body)
     except Exception as ex:
