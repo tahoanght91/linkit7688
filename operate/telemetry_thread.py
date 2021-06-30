9ba407da8f411dfadb1bc012b158aeefc35ae824
huyfr <61008601+huyfr@users.noreply.github.com>
2021-06-30 21:52:46 +0700

    comment check rc when send telemetry to tb

--- operate/telemetry_thread.py
+++ operate/telemetry_thread.py
@@ -1,8 +1,6 @@
 import time
-from random import randint
 
 from config import *
-from config.common import *
 
 
 def call():
@@ -12,9 +10,9 @@ def call():
             telemetry = format_telemetry(telemetries)
             for key, value in telemetry.items():
                 response = CLIENT.gw_send_telemetry(key, value)
-                LOGGER.info('RC of send telemetry to Thingsboard is: %s', str(response.rc()))
-                if response.rc() != 0:
-                    CLIENT.disconnect()
+                # LOGGER.info('RC of send telemetry to Thingsboard is: %s', str(response.rc()))
+                # if response.rc() != 0:
+                #     CLIENT.disconnect()
             LOGGER.info('Sent telemetry data')
             log_info = []
             for key, value in telemetries.items():
