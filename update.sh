01abf40d17bc29c903bed7a6c820f6db89bb029a
huyfr <61008601+huyfr@users.noreply.github.com>
2021-07-04 10:20:54 +0700

    edit update file

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
+#!/bin/ash
 /etc/init.d/7688 stop
 cp /IoT/linkit7688/config/devices.json /IoT
-if curl -LO https://github.com/huyfr/linkit7688/archive/refs/heads/huyfr.zip --max-time 5
+if curl -LO $1 --max-time 5
 then
-  echo 'Download new version successful!'
   mv /IoT/linkit7688 /
   unzip huyfr
   mv /IoT/linkit7688-huyfr linkit7688
