3c0b17ca4c1bd82254a71f3d4f8f87736d775a88
huyfr <61008601+huyfr@users.noreply.github.com>
2021-07-07 13:54:26 +0700

    delete unused code

--- operate/lcd_thread.py
+++ operate/lcd_thread.py
@@ -32,10 +32,9 @@ warningOld = ''
 last_stt_bt = 0
 
 
-# button_status[0]
 def call():
     try:
-        period = 0.5
+        period = 1
         while True:
             button = check_button(lcd_services)
             LOGGER.info('Send button value: %s', str(button))
@@ -45,167 +44,6 @@ def call():
             time.sleep(period)
     except Exception as ex:
         LOGGER.error('Error at call function in menu_thread with message: %s', ex.message)
-
-
-def check_key_code():
-    global time_pre
-    try:
-        # next_minute = datetime.now().strftime("%M")
-        result_check_input = check_lcd_service(lcd_services)
-        json_file = open(last_trace_lcd, )
-        last_trace = json.load(json_file)
-        a = last_trace['key_code']
-        b = last_trace['key_event']
-        LOGGER.info('OLD KEY CODE: %s', str(a))
-        if result_check_input.key_code > 0 and result_check_input.key_event > 0:
-            result_switch_lcd = switch_lcd_service(result_check_input)
-            set_last_trace(result_switch_lcd)
-            lcd_services.clear()
-        elif a > 0 and b > 0:
-            # if time_pre != next_minute and a != KEYCODE_11:
-            #     time_pre = next_minute
-            #     a = KEYCODE_11
-            #     b = EVENT_UP
-            #     last_trace = {'key_event': EVENT_UP, 'key_code': KEYCODE_11}
-            #     write_to_json(last_trace, last_trace_lcd)
-            check_last_display(a, b)
-    except Exception as ex:
-        LOGGER.error('Error at call function in check_key_code with message: %s', ex.message)
-
-
-def check_last_display(key_code, key_event):
-    try:
-        if key_code == KEYCODE_11 and key_event == EVENT_UP:
-            screen_main()
-        elif key_code == KEYCODE_12 and key_event == EVENT_UP:
-            check_alarm()
-    except Exception as ex:
-        LOGGER.error('Error at call function in check_last_display with message: %s', ex.message)
-
-
-# HungLq
-def read_to_json(fileUrl):
-    try:
-        json_file = open(fileUrl, )
-        json_info = json.load(json_file)
-    except Exception as ex:
-        LOGGER.error('Error at call function in read_to_json with message: %s', ex.message)
-    return json_info
-
-
-# HuyTQ
-def switch_lcd_service(input_lcd):
-    last_trace = Lcd()
-    global titleOld
-    global acmTempInOld
-    global acmTempOutOld
-    global acmHumidInOld
-    global warningOld
-    global timeOld
-    try:
-        key_event = input_lcd.key_event
-        key_code = input_lcd.key_code
-        if key_event == EVENT_UP:
-            if key_code == KEYCODE_11:
-                titleOld = ''
-                acmTempInOld = 0
-                acmTempOutOld = 0
-                acmHumidInOld = 0
-                warningOld = ''
-                timeOld = '61'
-                screen_main()
-            # elif key_code == KEYCODE_16:
-            #     last_trace.category = dct_lcd_menu['id']
-            #     last_trace.level = dct_lcd_menu_level['levelId']
-            #     last_trace.index_level1 = dct_lcd_menu_level_lv1[0]['index']
-            #     last_trace.name = dct_lcd_menu_level_lv1[0]['name']
-            #     last_trace.value = -1
-            #     last_trace.index_level2 = -1
-            # elif key_code == KEYCODE_14 or key_code == KEYCODE_34:
-            #     last_trace = navigate_lcd_service(key_code)
-            # elif key_code == KEYCODE_24:
-            #     last_trace = enter_lcd_service()
-            # elif key_code == KEYCODE_13:
-            #     pass
-            elif key_code == KEYCODE_12:
-                remove_json_file_alarm()
-                check_alarm()
-            elif key_event == EVENT_DOWN:
-                pass
-            elif key_event == EVENT_HOLD:
-                pass
-
-            last_trace.key_code = input_lcd.key_code
-            last_trace.key_event = input_lcd.key_event
-        else:
-            LOGGER.info('Key event != EVENT_UP so do nothing')
-    except Exception as ex:
-        LOGGER.info('Error at switch_lcd_service function with message: %s', ex.message)
-    return last_trace
-
-
-def enter_lcd_service():
-    last_trace = get_last_trace()
-    try:
-        if last_trace.category == KEYCODE_16:
-            if last_trace.level == MENU_LEVEL_1:
-                last_trace.name = dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['keys'][0]
-                last_trace.level = dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['levelId']
-                last_trace.index_level2 = dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['keys'].index(
-                    last_trace.name)
-            elif last_trace.level == MENU_LEVEL_2:
-                if last_trace.value == -1:
-                    value = shared_attributes.get(last_trace.name, default_data.data_dict['shared'][last_trace.name])
-                    last_trace.level = MENU_LEVEL_3
-                    last_trace.value = value
-            elif last_trace.level == MENU_LEVEL_3:
-                body = write_body_send_shared_attributes(last_trace.name, last_trace.value)
-                result = send_shared_attributes(body)
-                if result:
-                    last_trace.value = -1
-                    last_trace.level = MENU_LEVEL_2
-        else:
-            pass
-    except Exception as ex:
-        LOGGER.error('Error at confirm_lcd_service function with message: %s', ex.message)
-    return last_trace
-
-
-def navigate_lcd_service(key_code):
-    index = -1
-    last_trace = get_last_trace()
-    try:
-        if last_trace.category == KEYCODE_16 and last_trace.level == MENU_LEVEL_1:
-            if key_code == KEYCODE_14:
-                index = last_trace.index_level1 + 1
-                if index > dct_lcd_menu_level['maxIndex']:
-                    index = dct_lcd_menu_level['minIndex']
-            elif key_code == KEYCODE_34:
-                index = last_trace.index_level1 - 1
-                if index < dct_lcd_menu_level['minIndex']:
-                    index = dct_lcd_menu_level['maxIndex']
-            last_trace.index_level1 = index
-            last_trace.name = dct_lcd_menu_level_lv1[index]['name']
-        elif last_trace.category == KEYCODE_16 and last_trace.level == MENU_LEVEL_2:
-            if key_code == KEYCODE_14:
-                index = last_trace.index_level2 + 1
-                if index > dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['maxIndex']:
-                    index = dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['minIndex']
-            elif key_code == KEYCODE_34:
-                index = last_trace.index_level2 - 1
-                if index < dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['minIndex']:
-                    index = dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['maxIndex']
-            last_trace.index_level2 = index
-            last_trace.name = dct_lcd_menu_level_lv1[last_trace.index_level1]['lv2']['keys'][index]
-        elif last_trace.category == KEYCODE_16 and last_trace.level == MENU_LEVEL_3:
-            if key_code == KEYCODE_14:
-                index = last_trace.value + 1
-            elif key_code == KEYCODE_34:
-                index = last_trace.value - 1
-            last_trace.value = index
-    except Exception as ex:
-        LOGGER.error('Error at confirm_lcd_service function with message: %s', ex.message)
-    return last_trace
 
 
 def write_body_send_shared_attributes(key, value):
@@ -245,91 +83,6 @@ def extract_lcd_service(byte_data):
         LOGGER.error('Error at extract_lcd_service function with message: %s', ex.message)
 
 
-def get_last_trace():
-    lcd_last_trace = Lcd()
-    try:
-        json_file = open('./last_trace_lcd.json', )
-        dct_last_trace = json.load(json_file)
-        lcd_last_trace.key_code = dct_last_trace['key_code']
-        lcd_last_trace.key_event = dct_last_trace['key_event']
-        lcd_last_trace.category = dct_last_trace['category']
-        lcd_last_trace.level = dct_last_trace['level']
-        lcd_last_trace.index_level1 = dct_last_trace['index_level1']
-        lcd_last_trace.index_level2 = dct_last_trace['index_level2']
-        lcd_last_trace.value = dct_last_trace['value']
-        lcd_last_trace.name = dct_last_trace['name']
-    except Exception as ex:
-        LOGGER.error('Error at get_last_trace with message: %s', ex.message)
-    return lcd_last_trace
-
-
-def set_last_trace(input_lcd):
-    try:
-        dct_last_trace = input_lcd.__dict__
-        json_last_trace = json.dumps(dct_last_trace)
-        with io.open('./last_trace_lcd.json', 'wb') as last_trace_file:
-            last_trace_file.write(json_last_trace)
-        LOGGER.info('Command information just send: %s', dct_last_trace)
-    except Exception as ex:
-        LOGGER.error('Error at set_last_trace function with message: %s', ex.message)
-
-
-def check_lcd_service(dct_lcd_service):
-    key_code_checked = False
-    key_event_checked = False
-    input_lcd = Lcd()
-    try:
-        input_lcd.key_code = dct_lcd_service['key_code']
-        input_lcd.key_event = dct_lcd_service['key_event']
-
-        if input_lcd.key_code in LIST_KEY_CODE:
-            key_code_checked = True
-            LOGGER.info('Key code: %d, exist in LIST_KEY_CODE', input_lcd.key_code)
-        else:
-            LOGGER.info('Key code: %d not exists LIST_KEY_CODE', input_lcd.key_code)
-
-        if input_lcd.key_event in LIST_KEY_EVENT:
-            key_event_checked = True
-            LOGGER.info('Key event: %d exists in LIST_KEY_EVENT', input_lcd.key_event)
-        else:
-            LOGGER.info('Key event: %d not exists in LIST_KEY_EVENT', input_lcd.key_event)
-
-        if key_code_checked and key_event_checked:
-            LOGGER.info('Check key code & key event successful')
-        else:
-            lcd_services.clear()
-            LOGGER.info('Fail while check ')
-    except Exception as ex:
-        LOGGER.error('Error at check_lcd_service function with message: %s', ex.message)
-    return input_lcd
-
-
-#Nguyenvq
-def remove_json_file_alarm():
-    try:
-        file_json = read_to_json(last_cmd_alarm)
-        file_json['row1'] = ""
-        file_json['row2'] = ""
-        file_json['row3'] = ""
-        write_to_json(file_json, last_cmd_alarm)
-    except Exception as ex:
-        LOGGER.error('Error at remove_json_file_alarm function with message: %s', ex.message)
-
-
-def int_file_trace():
-    try:
-        file = {'key_event': EVENT_UP, 'key_code': KEYCODE_11}
-        write_to_json(file, last_trace_lcd)
-    except Exception as ex:
-        LOGGER.error('Error at remove_json_file_alarm function with message: %s', ex.message)
-
-
-def run_repeat_cmd_lcd(str_cmd):
-    num = 1
-    for x in range(num):
-        cmd_lcd[UPDATE_VALUE] = str_cmd
-        time.sleep(0.1)
-
 def check_button(bt_info):
     global last_stt_bt
     try:
@@ -351,7 +104,8 @@ def check_button(bt_info):
                 LOGGER.info('return button value: %s', LOG_BUTTON[str(button)])
             return button
         else:
-            return -1
             LOGGER.info('Button status: No action')
+            return -1
     except Exception as ex:
         LOGGER.error('check_button function error: %s', ex.message)
+
