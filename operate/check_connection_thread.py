8311af91bf181d912d9120783e408bd7213c9604
huyfr <61008601+huyfr@users.noreply.github.com>
2021-07-12 17:22:10 +0700

    change solution check internet in check_connection_thread.py

--- operate/check_connection_thread.py
+++ operate/check_connection_thread.py
@@ -1,5 +1,6 @@
 import json
 import os
+import socket
 import subprocess
 import time
 
@@ -11,27 +12,44 @@ from config.common_api import *
 COMMAND_RESET_SERVICE_7688 = 'reboot'
 url_check_connection = PREFIX + DOMAIN + API_CHECK_CONNECTION
 params = {'gatewayId': CLIENT_ID}
+timeout = 3
+host = 'smartsite.mobifone.vn'
+port = 443
+
+
+# def call():
+#     period = 120
+#     while True:
+#         try:
+#             time.sleep(period)
+#             LOGGER.info('Pinging to Google: %s', subprocess.check_output(['ping', '-c', '4', 'google.com']))
+#             response = requests.get(url=url_check_connection, params=params)
+#             if response.status_code == 200:
+#                 LOGGER.info('Send check connection request to Smartsite successful!')
+#                 active = json.loads(response.content)['result']
+#                 if active:
+#                     LOGGER.info('Gateway is online!')
+#                 else:
+#                     LOGGER.info('Gateway is offline!')
+#                     CLIENT.disconnect()
+#             else:
+#                 LOGGER.info('Response from Smartsite is not 200')
+#                 CLIENT.disconnect()
+#         except Exception as ex:
+#             LOGGER.info('Error when check connection with message: %s', ex.message)
+#             CLIENT.disconnect()
 
 
 def call():
-    period = 120
+    period = 10
     while True:
         try:
             time.sleep(period)
             LOGGER.info('Pinging to Google: %s', subprocess.check_output(['ping', '-c', '4', 'google.com']))
-            response = requests.get(url=url_check_connection, params=params)
-            if response.status_code == 200:
-                LOGGER.info('Send check connection request to Smartsite successful!')
-                active = json.loads(response.content)['result']
-                if active:
-                    LOGGER.info('Gateway is online!')
-                else:
-                    LOGGER.info('Gateway is offline!')
-                    CLIENT.disconnect()
-            else:
-                LOGGER.info('Response from Smartsite is not 200')
-                CLIENT.disconnect()
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+            LOGGER.info('Gateway is online !!!')
         except Exception as ex:
-            LOGGER.info('Error when check connection with message: %s', ex.message)
+            LOGGER.debug('Gateway is offline !!!')
+            LOGGER.error('Error when check connection with message: %s', ex.message)
             CLIENT.disconnect()
-
