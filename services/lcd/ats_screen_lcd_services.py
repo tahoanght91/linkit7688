83d50ce8944440c06c659788f10e8a18987f236e
nguyenvq <nguyenvq@dft.vn>
2021-07-14 15:01:29 +0700

    sua file ats

--- services/lcd/ats_screen_lcd_services.py
+++ services/lcd/ats_screen_lcd_services.py
@@ -2,39 +2,68 @@ from config import *
 from config.common import UPDATE_VALUE
 from config.common_lcd_services import *
 
+last_cmd_lcd = './last_cmd_screen.json'
 
-def get_title_ats():
+
+def get_title_ats(row1):
     from control import process_cmd_lcd
     try:
         show = 'THONG TIN ATS'
-        process_cmd_lcd(ROW_1, UPDATE_VALUE, show)
+        if row1 != show:
+            process_cmd_lcd(ROW_1, UPDATE_VALUE, show)
+            row1 = show
     except Exception as ex:
         LOGGER.error('Error at get_title_ats function with message: %s', ex.message)
+    return row1
 
 
-def get_info_ats():
+def get_info_ats(row2_old, row3_old, row4_old):
     from control import process_cmd_lcd
     try:
+        list_row = []
         row2 = 'Ket noi' if client_attributes.get('atsConnect') > 0 else 'Mat ket noi'
         row3 = 'Nguon: May phat' if client_attributes.get('atsContactorGenState') > 0 else 'Nguon: Dien luoi'
         row4 = 'Che do: ' + str(telemetries.get('atsState', default=''))
-        process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
-        process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
-        process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+        if row2_old != row2:
+            process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
+            row2_old = row2
+        if row3_old != row3:
+            process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
+            row3_old = row3
+        if row4_old != row4:
+            process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+            row4_old = row4
         LOGGER.info('ATS SCREEN : %s', 1)
+
+        list_row.append(row2_old)
+        list_row.append(row3_old)
+        list_row.append(row4_old)
+        return list_row
     except Exception as ex:
         LOGGER.error('Error at get_info_ats function with message: %s', ex.message)
 
 
 def get_detail_screen1_ats():
     from control import process_cmd_lcd
+    from control.utils import read_to_json, write_to_json
+
     try:
+        all_row = read_to_json(last_cmd_lcd)
+
         row2 = 'Dien luoi: ' + str(telemetries.get('atsAcState', default=''))
         row3 = 'May phat: ' + str(telemetries.get('atsGenState', default=''))
         row4 = 'Che do M.Phat: ' + str(telemetries.get('atsState', default=''))
-        process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
-        process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
-        process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+        if all_row['row2'] != row2:
+            process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
+            all_row['row2'] = row2
+        if all_row['row3'] != row3:
+            process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
+            all_row['row3'] = row3
+        if all_row['row4'] != row4:
+            process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+            all_row['row4'] = row4
+
+        write_to_json(all_row, last_cmd_lcd)
         LOGGER.info('ATS SCREEN : %s', 2)
     except Exception as ex:
         LOGGER.error('Error at get_detail_ats function with message: %s', ex.message)
@@ -42,7 +71,10 @@ def get_detail_screen1_ats():
 
 def get_detail_screen2_ats():
     from control import process_cmd_lcd
+    from control.utils import read_to_json, write_to_json
     try:
+        all_row = read_to_json(last_cmd_lcd)
+
         row2 = telemetries.get('atsVacP1', default=0) + 'V ' + telemetries.get('atsVacP2',
                                                                                default=0) + 'V ' + telemetries.get(
             'atsVacP3', default=0) + 'V'
@@ -52,17 +84,26 @@ def get_detail_screen2_ats():
         row4 = telemetries.get('atsVloadP1', default=0) + 'V ' + telemetries.get('atsVloadP2',
                                                                                  default=0) + 'V ' + telemetries.get(
             'atsVloadP3', default=0) + 'V'
-        process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
-        process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
-        process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+        if all_row['row2'] != row2:
+            process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
+            all_row['row2'] = row2
+        if all_row['row3'] != row3:
+            process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
+            all_row['row3'] = row3
+        if all_row['row4'] != row4:
+            process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+            all_row['row4'] = row4
         LOGGER.info('ATS SCREEN : %s', 3)
+        write_to_json(all_row, last_cmd_lcd)
     except Exception as ex:
         LOGGER.error('Error at get_detail_screen2_ats function with message: %s', ex.message)
 
 
 def get_detail_screen3_ats():
     from control import process_cmd_lcd
+    from control.utils import read_to_json, write_to_json
     try:
+        all_row = read_to_json(last_cmd_lcd)
         row2 = telemetries.get('atsIloadP1', default=0) + 'A ' + telemetries.get('atsIloadP2',
                                                                                  default=0) + 'A ' + telemetries.get(
             'atsIloadP3', default=0) + 'A'
@@ -73,17 +114,26 @@ def get_detail_screen3_ats():
         row4 = telemetries.get('atsVacFreq', default=0) + 'Hz ' + telemetries.get('atsVgenFreq',
                                                                                   default=0) + 'Hz ' + telemetries.get(
             'atsVloadFreq', default=0) + 'Hz'
-        process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
-        process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
-        process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+        if all_row['row2'] != row2:
+            process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
+            all_row['row2'] = row2
+        if all_row['row3'] != row3:
+            process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
+            all_row['row3'] = row3
+        if all_row['row4'] != row4:
+            process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+            all_row['row4'] = row4
         LOGGER.info('ATS SCREEN : %s', 4)
+        write_to_json(all_row, last_cmd_lcd)
     except Exception as ex:
         LOGGER.error('Error at get_detail_screen3_ats function with message: %s', ex.message)
 
 
 def get_detail_screen4_ats():
     from control import process_cmd_lcd
+    from control.utils import read_to_json, write_to_json
     try:
+        all_row = read_to_json(last_cmd_lcd)
         row2 = telemetries.get('mccDcBat1Temp', default=0) + 'C ' + telemetries.get('mccDcBat2Temp',
                                                                                     default=0) + 'C ' + telemetries.get(
             'mccDcBat3Temp', default=0) + 'C'
@@ -93,17 +143,26 @@ def get_detail_screen4_ats():
         row4 = telemetries.get('mccDcV2', default=0) + 'V ' + telemetries.get('mccDcI2',
                                                                               default=0) + 'A ' + telemetries.get(
             'mccDcP2', default=0) + 'W'
-        process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
-        process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
-        process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+        if all_row['row2'] != row2:
+            process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
+            all_row['row2'] = row2
+        if all_row['row3'] != row3:
+            process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
+            all_row['row3'] = row3
+        if all_row['row4'] != row4:
+            process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+            all_row['row4'] = row4
         LOGGER.info('ATS SCREEN : %s', 5)
+        write_to_json(all_row, last_cmd_lcd)
     except Exception as ex:
         LOGGER.error('Error at get_detail_screen4_ats function with message: %s', ex.message)
 
 
 def get_detail_screen5_ats():
     from control import process_cmd_lcd
+    from control.utils import read_to_json, write_to_json
     try:
+        all_row = read_to_json(last_cmd_lcd)
         row2 = telemetries.get('mccDcV3', default=0) + 'V ' + telemetries.get('mccDcI3',
                                                                               default=0) + 'A ' + telemetries.get(
             'mccDcP3', default=0) + 'W'
@@ -113,17 +172,37 @@ def get_detail_screen5_ats():
         row4 = telemetries.get('mccDcV5', default=0) + 'V ' + telemetries.get('mccDcI5',
                                                                               default=0) + 'A ' + telemetries.get(
             'mccDcP5', default=0) + 'W'
-        process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
-        process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
-        process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+        if all_row['row2'] != row2:
+            process_cmd_lcd(ROW_2, UPDATE_VALUE, str(row2))
+            all_row['row2'] = row2
+        if all_row['row3'] != row3:
+            process_cmd_lcd(ROW_3, UPDATE_VALUE, str(row3))
+            all_row['row3'] = row3
+        if all_row['row4'] != row4:
+            process_cmd_lcd(ROW_4, UPDATE_VALUE, str(row4))
+            all_row['row4'] = row4
         LOGGER.info('ATS SCREEN : %s', 6)
+        write_to_json(all_row, last_cmd_lcd)
     except Exception as ex:
         LOGGER.error('Error at get_detail_screen5_ats function with message: %s', ex.message)
 
 
 def get_screen_ats():
+    from control.utils import read_to_json, write_to_json
     try:
-        get_title_ats()
-        get_info_ats()
+        all_row = read_to_json(last_cmd_lcd)
+        row1 = all_row['row1']
+        row2 = all_row['row2']
+        row3 = all_row['row3']
+        row4 = all_row['row4']
+        # enter func display
+        row1 = get_title_ats(row1)
+        list_row = get_info_ats(row2, row3, row4)
+        # save to file
+        all_row['row1'] = row1
+        all_row['row2'] = list_row[0]
+        all_row['row3'] = list_row[1]
+        all_row['row4'] = list_row[2]
+        write_to_json(all_row, last_cmd_lcd)
     except Exception as ex:
         LOGGER.error('Error at get_screen_ats function with message: %s', ex.message)
