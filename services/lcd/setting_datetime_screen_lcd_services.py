ace445aadcedc5306bfcec1c16404046817cd96f
Thanh Cong <thanhcong1395@gmail.com>
2021-07-15 16:59:49 +0700

    fix timedate setting

--- services/lcd/setting_datetime_screen_lcd_services.py
+++ services/lcd/setting_datetime_screen_lcd_services.py
@@ -4,8 +4,8 @@ from config.common import *
 from config.common_lcd_services import *
 
 # Common variables
-date = ['_', '_', '/', ' ', '_', '_', '/', ' ', '_', '_', '_', '_']
-time = ['_', '_', ':', ' ', '_', '_']
+date = ['_', '_', '/', '_', '_', '/', '_', '_', '_', '_']
+time = ['_', '_', ':', '_', '_']
 level_at_index = [0] * 12
 level_at_index_time = [0] * 6
 number = [str(u) for u in range(10)]
@@ -14,6 +14,25 @@ cursor_idx = 0
 cursor_idx_time = 0
 confirm_idx = 0
 confirm_status = False
+last_time_setting_screen_index = -1
+time_setting_screen_index = 0
+go_setting_flag = False
+
+time_setting_print = {
+    0: {
+        'row1': 'THOI GIAN',
+        'row2': '> Ngay',
+        'row3': 'Ngay gio',
+        'row4': ''
+    },
+    1: {
+        'row1': 'THOI GIAN',
+        'row2': 'Ngay',
+        'row3': '> Gio',
+        'row4': ''
+    }
+}
+
 
 
 def call_screen_confirm(p_idx):
@@ -47,9 +66,9 @@ def date_setting_process(button):
             if button == OK:
                 if confirm_idx == 0:
                     try:
-                        os.system("""date +%Y%m%d "{year}{month}{day}" """.format(year=''.join(date[8:12]),
-                                                                                  month=''.join(date[4:6]),
-                                                                                  day=''.join(date[0:2])))
+                        os.system("""date +%Y%m%d "{year}{month}{day}" """.format(year=''.join(date[6:9]),
+                                                                                  month=''.join(date[3:4]),
+                                                                                  day=''.join(date[0:1])))
                     except Exception as ex:
                         LOGGER.error('Error at set datetime to os in os.system with message: %s', ex.message)
                     confirm_status = False
@@ -70,6 +89,7 @@ def date_setting_process(button):
                 if ['_', '_', '/', ' ', '_', '_', '/', ' ', '_', '_', '_', '_'] == date:
                     process_cmd_lcd(ROW_1, UPDATE_VALUE, 'NGAY')
                     process_cmd_lcd(ROW_2, UPDATE_VALUE, ''.join(date))
+                    process_cmd_lcd(ROW_3, UPDATE_VALUE, '')
                 elif "_" not in date:
                     confirm_status = True
                     call_screen_confirm(0)
@@ -90,8 +110,8 @@ def date_setting_process(button):
                     date[cursor_idx] = number[level_at_index[cursor_idx]]
                     process_cmd_lcd(ROW_2, UPDATE_VALUE, ''.join(date))
                 elif button == RIGHT:
-                    if cursor_idx == 1 or cursor_idx == 5:
-                        cursor_idx += 3
+                    if cursor_idx == 1 or cursor_idx == 4:
+                        cursor_idx += 2
                     else:
                         cursor_idx += 1
     except Exception as ex:
@@ -107,8 +127,8 @@ def time_setting_process(button):
             if button == OK:
                 if confirm_idx == 0:
                     try:
-                        os.system("""date +%T -s "{hour}:{minute}" """.format(hour=''.join(time[0:2]),
-                                                                              minute=''.join(time[4:6]), ))
+                        os.system("""date +%T -s "{hour}:{minute}" """.format(hour=''.join(time[0:1]),
+                                                                              minute=''.join(time[3:4])))
                     except Exception as ex:
                         LOGGER.error('Error at call function in os.system in 113 with message: %s', ex.message)
                     confirm_status = False
@@ -147,9 +167,41 @@ def time_setting_process(button):
                     time[cursor_idx_time] = number[level_at_index_time[cursor_idx_time]]
                     process_cmd_lcd(ROW_2, UPDATE_VALUE, ''.join(time))
                 elif button == RIGHT:
-                    if cursor_idx_time == 1 or cursor_idx_time == 5:
-                        cursor_idx_time += 3
+                    if cursor_idx_time == 1:
+                        cursor_idx_time += 2
                     else:
                         cursor_idx_time += 1
     except Exception as ex:
         LOGGER.error('Error at call function in time_setting_process with message: %s', ex.message)
+
+
+def datetime_setting(button):
+    from control import process_cmd_lcd
+    global last_time_setting_screen_index, time_setting_screen_index, ok_time, go_setting_flag
+
+    try:
+        function = {
+            0: date_setting_process,
+            1: time_setting_process
+        }
+        if go_setting_flag is True:
+            return function[time_setting_screen_index](button)
+        if button == DOWN:
+            time_setting_screen_index = 1
+        elif button == UP:
+            time_setting_screen_index = 0
+        elif button == OK:
+            if ok_time > 0:
+                go_setting_flag = True
+                return function[time_setting_screen_index](button)
+            else:
+                ok_time = 1
+        if last_time_setting_screen_index != time_setting_screen_index:
+            process_cmd_lcd(ROW_1, UPDATE_VALUE, time_setting_print[time_setting_screen_index]['row1'])
+            process_cmd_lcd(ROW_2, UPDATE_VALUE, time_setting_print[time_setting_screen_index]['row2'])
+            process_cmd_lcd(ROW_3, UPDATE_VALUE, time_setting_print[time_setting_screen_index]['row3'])
+            process_cmd_lcd(ROW_4, UPDATE_VALUE, time_setting_print[time_setting_screen_index]['row4'])
+            last_time_setting_screen_index = time_setting_screen_index
+        LOGGER.info('finish time_setting function, time_setting_screen_index: %s', str(time_setting_screen_index))
+    except Exception as ex:
+        LOGGER.error('time_setting function error: %s', ex.message)
