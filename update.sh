3f5681f37e2077ad814589382fb1bf1d757e559f
huyfr <61008601+huyfr@users.noreply.github.com>
2021-07-22 11:34:25 +0700

    fix(update.sh): add params into file
    - add one more param to file update.sh
    - add more echo

--- update.sh
+++ update.sh
@@ -7,18 +7,22 @@ cp /IoT/linkit7688/last_rfid_card_code.json /IoT
 cp /IoT/linkit7688/lcd_setting_data_file.json /IoT
 if curl -LO $1 --max-time 5
 then
+  echo 'Download source code successful'
   mv /IoT/linkit7688 /
-  unzip huyfr
-  mv /IoT/linkit7688-huyfr linkit7688
+  unzip $2
+  echo 'Unzip successful'
+  mv /IoT/linkit7688-$2 linkit7688
   cp /IoT/devices.json /IoT/linkit7688/config/
   cp /IoT/last_cmd_alarm.json /IoT/linkit7688/
   cp /IoT/last_cmd_network.json /IoT/linkit7688/
   cp /IoT/last_rfid_card_code.json /IoT/linkit7688/
   cp /IoT/lcd_setting_data_file.json /IoT/linkit7688/
   cp /IoT/linkit7688/update.sh /IoT
+  echo 'Copy 6 files to linkit7688 successful'
   chmod +x /IoT/update.sh
   rm -rf /linkit7688
-  rm -rf /IoT/huyfr.zip
+  rm -rf /IoT/$2.zip
+  echo 'Remove old version linkit7688 and source code (.zip) successful'
   /etc/init.d/7688 start
 else
   echo 'CURL failed'
