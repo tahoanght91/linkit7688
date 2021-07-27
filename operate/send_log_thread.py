61619dd634cd58acbae1854df50818191ce9827a
huyfr <huytran059@gmail.com>
2021-07-27 09:14:18 +0700

    fix(send_log_thread.py): edit all thread
    - edit function call.
    - add new function check_log_exist
    - add new function detect_new_log
    - refactor function copy_log
    - edit function write_body_send_log
    - edit function send_log_smartsite

--- operate/send_log_thread.py
+++ operate/send_log_thread.py
@@ -8,47 +8,63 @@ import requests
 
 from config import LOGGER, device_config
 from config.common_api import API_SEND_LOG, PREFIX, DOMAIN
+from control.utils import read_to_json
 
 MIN_SIZE_FILE = 1468006.4
 MAX_SIZE_FILE = 1572864
 TIME_PERIOD = 30
 LOG_PATH = './app.log.1'
 DESTINATION = './log'
+LAST_UPDATE_LOG_PATH = './last_update_log.json'
 url_send_log = PREFIX + DOMAIN + API_SEND_LOG
 
 
 def call():
     period = TIME_PERIOD
     while True:
-        resp_get_size = get_size_log(LOG_PATH)
-        resp_copy_log = copy_log(resp_get_size[0], resp_get_size[2])
-        if resp_copy_log[0] and resp_copy_log[1]:
-            resp_write_body = write_body_send_log(resp_copy_log[2])
-            if len(resp_write_body[0]) > 0 and len(resp_write_body[1]) > 0:
-                resp_send_log = send_log_smartsite(resp_write_body[0], resp_write_body[1])
+        existence = check_log_exist(LAST_UPDATE_LOG_PATH, LOG_PATH)
+        if existence:
+            detected = detect_new_log(LAST_UPDATE_LOG_PATH, LOG_PATH)
+            if detected:
+                copied = copy_log(LOG_PATH)
+                if copied[0] and copied[1]:
+                    body = write_body_send_log(copied[2])
+                    if len(body) > 0:
+                        response = send_log_smartsite(body[0], body[-1])
         time.sleep(period)
 
 
-def get_size_log(path):
-    size = -1
+def check_log_exist(last_update_log_path, path):
     result = False
     try:
-        size = os.path.getsize(path)
-        if MIN_SIZE_FILE <= size <= MAX_SIZE_FILE:
+        if os.path.exists(path):
             result = True
     except Exception as ex:
-        LOGGER.warning('Error at get_size_log function with message: %s', ex.message)
-    return result, size, path
+        LOGGER.warning('Error at check_log_exist function with message: %s', ex.message)
+    return result
 
 
-def copy_log(result, source):
+def detect_new_log(last_update_log_path, log_path):
+    result = False
+    try:
+        last_update_file = read_to_json(last_update_log_path)
+        last_updated_date = last_update_file['updateDate']
+        updated_date = os.path.getmtime(log_path)
+        if updated_date > last_updated_date:
+            result = True
+    except Exception as ex:
+        LOGGER.warning('Error at detect_new_log function with message: %s', ex.message)
+    return result
+
+
+def copy_log(source):
     copied = False
     renamed = False
     destination = ''
     try:
         now = datetime.now()
         new_name = now.strftime('%d-%m-%Y-%H-%M-%S') + '.txt'
-        if result and os.path.exists(source):
+        if os.path.exists(source):
             shutil.copy(source, DESTINATION)
             copied = True
             LOGGER.debug('Copy log successful!')
@@ -66,24 +82,24 @@ def copy_log(result, source):
 
 
 def write_body_send_log(path_file):
-    params = {}
     files = {}
+    data = {}
     try:
         gateway_id = device_config['device_id']
-        files = {"uploadFile": open(path_file, 'rb')}
-        params = {"gatewayId": gateway_id}
-        LOGGER.info('Body send log to Smartsite: %s', params)
+        data = {'idTram': gateway_id}
+        files = {"file": open(path_file, 'rb')}
+        LOGGER.info('Params send log to Smartsite: %s', files)
     except Exception as ex:
         LOGGER.warning('Error at write_body_send_log function with message: %s', ex.message)
-    return params, files
+    return data, files
 
 
-def send_log_smartsite(params, files):
+def send_log_smartsite(data, files):
     result = False
     try:
-        if isinstance(params, dict) and isinstance(files, dict):
-            if len(params) > 0 and len(files) > 0:
-                response = requests.post(url_send_log, params=params, files=files)
+        if isinstance(data, dict) and isinstance(files, dict):
+            if len(data) > 0 and len(files) > 0:
+                response = requests.post(url_send_log, data=data, files=files)
                 if response.status_code == 200:
                     result = True
                     LOGGER.info('Send log to Smartsite successful!')
