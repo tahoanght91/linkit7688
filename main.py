9c8848c86c751e7d99fff05aaed033ad547348fc
huyfr <61008601+huyfr@users.noreply.github.com>
2021-07-30 09:57:54 +0700

    feat(edit log level): get log level on server

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
 try:
@@ -12,8 +12,12 @@ from operate import main_thread
 
 
 def main():
+    from config import shared_attributes, default_data
+    temp_level = shared_attributes.get('mccLogLevel', default_data.mccLogLevel)
+    from control.utils import validate_log_level
+    level = validate_log_level(temp_level)
     root = logging.getLogger('App')
-    root.setLevel(logging.DEBUG)
+    root.setLevel(level)
     size_bytes = 1500000  # bytes
     handler = RotatingFileHandler('app.log', mode='a', maxBytes=size_bytes, backupCount=1, encoding=None, delay=False)
     formatter = logging.Formatter('%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s() - %(levelname)s - %(message)s')
